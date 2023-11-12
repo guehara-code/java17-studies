4825a21aba500240e25f7dc75894beef1d0c4492
Gilberto <gil_uehara@yahoo.com.br>
2023-11-12 16:27:28 -0300

    more studies

--- _07-01-ClassesPart1/src/Main.java
+++ _07-01-ClassesPart1/src/Main.java
@@ -2,10 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car();
-        car.setMake("Porsche");
+        car.setMake("Maserati");
+        car.setModel("Carrera");
+        car.setDoors(2);
+        car.setConvertible(true);
+        car.setColor("black");
 
         System.out.println("make: " + car.getMake());
         System.out.println("model: " + car.getModel());
+
+        car.describeCar();
+
+        Car targa = new Car();
+        car.setMake("Porsche");
+        car.setModel("Targa");
+        car.setDoors(2);
+        car.setConvertible(false);
+        car.setColor("red");
+
         car.describeCar();
     }
 }
