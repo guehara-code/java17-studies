0987535afe5697c9f2997531d8d261d875bda133
Gilberto <gil_uehara@yahoo.com.br>
2023-11-15 08:13:03 -0300

    more studies

---
+++ _07-03-ConstructorChallenge/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Customer customer = new Customer("Tim", 1000, "tim@email.com");
+        System.out.println(customer.getName());
+        System.out.println(customer.getCreditLimit());
+        System.out.println(customer.getEmail());
+
+        Customer secondCustomer = new Customer();
+        System.out.println(secondCustomer.getName());
+        System.out.println(secondCustomer.getCreditLimit());
+        System.out.println(secondCustomer.getEmail());
+    }
+}
