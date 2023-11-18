b9b3d69a1d3592d084df1306489a4bc74d6c3844
Gilberto <gil_uehara@yahoo.com.br>
2023-11-18 09:02:21 -0300

    more studies

---
+++ _07-05-ObjectClass/src/Main.java
@@ -0,0 +1,30 @@
+public class Main extends Object {
+    public static void main(String[] args) {
+
+        Student max = new Student("Max", 21);
+        System.out.println(max.toString());
+    }
+}
+
+class Student {
+    private String name;
+    private int age;
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+//    public String toString() {
+//        return super.toString();
+//    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
