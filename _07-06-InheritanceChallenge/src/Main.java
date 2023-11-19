f31e296c5d689c8094687f4cef466900d90841f8
Gilberto <gil_uehara@yahoo.com.br>
2023-11-19 09:29:26 -0300

    more studies

--- _07-06-InheritanceChallenge/src/Main.java
+++ _07-06-InheritanceChallenge/src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         Employee tim = new Employee("Tim", "11/11/1985", "01/01/2020");
@@ -13,5 +15,11 @@ public class Main {
 
         john.retire();
         System.out.println("Joe's Pension check = $" + john.collectPay());
+
+        HourlyEmployee mary = new HourlyEmployee("Mary", "05/05/1970", "03/03/2021",
+                15);
+        System.out.println(mary);
+        System.out.println("Mary's Paycheck = $" + mary.collectPay());
+        System.out.println("Mary's Holiday Pay = $" + mary.getDoublePay());
     }
 }
