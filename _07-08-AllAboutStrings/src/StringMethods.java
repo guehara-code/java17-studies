32f4304c1bcb1b9bcfd548776a423a86302a52ee
Gilberto <gil_uehara@yahoo.com.br>
2023-11-21 08:52:12 -0300

    more studies

--- _07-08-AllAboutStrings/src/StringMethods.java
+++ _07-08-AllAboutStrings/src/StringMethods.java
@@ -20,5 +20,23 @@ public class StringMethods {
 
         newDate = "25" + "/" + "11" + "/" + "1982";
         System.out.println("newDate = " + newDate);
+
+        newDate = "25".concat("/").concat("11").concat("/").concat("1982");
+        System.out.println("newDate = " + newDate);
+
+        System.out.println(newDate.replace('/', '-'));
+        System.out.println(newDate.replace("2", "00"));
+
+        System.out.println(newDate.replaceFirst("/", "-"));
+        System.out.println(newDate.replace("/", "---"));
+
+        System.out.println("ABC\n".repeat(3));
+        System.out.println("-".repeat(20));
+
+        System.out.println("ABC\n".repeat(3).indent(8));
+        System.out.println("-".repeat(20));
+
+        System.out.println("    ABC\n".repeat(3).indent(-2));
+        System.out.println("-".repeat(20));
      }
 }
