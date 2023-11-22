72482cba095fab6268d9fcbac3955fb69a74c2aa
Gilberto <gil_uehara@yahoo.com.br>
2023-11-22 16:24:09 -0300

    more studies

--- _08-01-ComputerFactory/src/Main.java
+++ _08-01-ComputerFactory/src/Main.java
@@ -6,8 +6,10 @@ public class Main {
         PersonalComputer thePC = new PersonalComputer("2208", "Dell",
                 theCase, theMonitor, theMotherboard);
 
-        thePC.getMonitor().drawPixelArt(10, 10, "red");
-        thePC.getMotherboard().loadProgram("Windows OS");
-        thePC.getComputerCase().pressPowerButton();
+//        thePC.getMonitor().drawPixelArt(10, 10, "red");
+//        thePC.getMotherboard().loadProgram("Windows OS");
+//        thePC.getComputerCase().pressPowerButton();
+
+        thePC.powerUp();
     }
 }
