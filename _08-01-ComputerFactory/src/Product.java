11d05e6a2a1bc62631557725b6ad9992e20d5116
Gilberto <gil_uehara@yahoo.com.br>
2023-11-22 16:09:21 -0300

    more studies

--- _08-01-ComputerFactory/src/Product.java
+++ _08-01-ComputerFactory/src/Product.java
@@ -12,8 +12,23 @@ public class Product {
 }
 
 class Monitor extends Product {
+
+    private int size;
+    private String resolution;
+
+    public Monitor(String model, String manufacturer, int size, String resolution) {
+        super(model, manufacturer);
+        this.size = size;
+        this.resolution = resolution;
+    }
+
     public Monitor(String model, String manufacturer) {
         super(model, manufacturer);
+    }
+
+    public void drawPixelArt(int x, int y, String color) {
+        System.out.println(String.format(
+                "Drawing pixel at %d,%d in color %s " , x, y, color));
     }
 }
 
@@ -40,7 +55,19 @@ class Motherboard extends Product {
 }
 
 class ComputerCase extends Product {
+
+    private String powerSupply;
+
+    public ComputerCase(String model, String manufacturer, String powerSupply) {
+        super(model, manufacturer);
+        this.powerSupply = powerSupply;
+    }
+
     public ComputerCase(String model, String manufacturer) {
         super(model, manufacturer);
+    }
+
+    public void pressPowerButton() {
+        System.out.println("Power button pressed");
     }
 }
