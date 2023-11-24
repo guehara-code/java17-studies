04a3f2203ea20a06bd7c947ad148aca058bbd10e
Gilberto <gil_uehara@yahoo.com.br>
2023-11-24 06:50:28 -0300

    more studies

---
+++ _08-03-Encapsulation/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.name = "Tim";
+        player.health = 20;
+        player.weapon = "Sword";
+
+        int damage = 10;
+        player.loseHeath(damage);
+        System.out.println("Remaining health = " + player.healthRemaining());
+
+        player.loseHeath(11);
+        System.out.println("Remaining health = " + player.healthRemaining());
+    }
+}
