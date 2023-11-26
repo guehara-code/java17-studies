7b47dc5fc00124c4284470f4b66849b96b37db7d
Gilberto <gil_uehara@yahoo.com.br>
2023-11-26 08:57:06 -0300

    more studies

--- _08-05-Polymorphism/src/Main.java
+++ _08-05-Polymorphism/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        Movie theMovie = new Adventure("Star Wars");
+//        Movie theMovie = new Adventure("Star Wars");
+        Movie theMovie = Movie.getMovie("" +
+                "Science", "Star Wars");
         theMovie.watchMovie();
 
     }
