bee041b7f16d0ed68796330629d2336fd4198f06
Gilberto <gil_uehara@yahoo.com.br>
2023-11-27 08:32:46 -0300

    more studies

---
+++ _08-06-PolymorphismChallenge/src/Car.java
@@ -0,0 +1,35 @@
+public class Car {
+    private String description;
+
+    public Car(String description) {
+        this.description = description;
+    }
+
+    public void startEngine() {
+        System.out.println("Car -> startEngine");
+    }
+
+    protected void runEngine() {
+        System.out.println("Car -> runEngine");
+    }
+
+    public void drive() {
+        System.out.println("Car -> driving, type is " + getClass().getSimpleName());
+        runEngine();
+    }
+}
+
+class GasPoweredCar extends Car {
+    private double avgPerLiter;
+    private int cylinders = 6;
+
+    public GasPoweredCar(String description) {
+        super(description);
+    }
+
+    public GasPoweredCar(String description, double avgPerLiter, int cylinders) {
+        super(description);
+        this.avgPerLiter = avgPerLiter;
+        this.cylinders = cylinders;
+    }
+}
