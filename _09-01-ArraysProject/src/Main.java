f48750f5c2b914550674bfc664a6473971242e73
Gilberto <gil_uehara@yahoo.com.br>
2023-12-01 13:56:20 -0300

    more studies

--- _09-01-ArraysProject/src/Main.java
+++ _09-01-ArraysProject/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] myIntArray = new int[10];
@@ -15,10 +17,37 @@ public class Main {
         System.out.println("length of array = " + arrayLength);
         System.out.println("last = " + firstTen[arrayLength - 1]);
 
-        int [] newArray;
+        int[] newArray;
         newArray = new int[]{5, 4, 3, 2, 1};
         for (int i = 0; i < newArray.length; i++) {
             System.out.print(newArray[i] + " ");
         }
+
+        System.out.println("");
+
+        newArray = new int[5];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = newArray.length - i;
+        }
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i] + " ");
+        }
+
+        System.out.println();
+        for (int element : newArray) {
+            System.out.print(element + " ");
+        }
+
+        System.out.println();
+        System.out.println(Arrays.toString(newArray));
+        Object objectVariable = newArray;
+        if (objectVariable instanceof int[]) {
+            System.out.println("objectVariable is really an int array");
+        }
+
+        Object[] objectArray = new Object[3];
+        objectArray[0] = "Hello";
+        objectArray[1] = new StringBuilder("World");
+        objectArray[2] = newArray;
     }
 }
