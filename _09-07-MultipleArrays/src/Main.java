f3cd0517e4697b1f5b69c7953d72141199d89a80
Gilberto <gil_uehara@yahoo.com.br>
2023-12-04 14:14:28 -0300

    more studies

---
+++ _09-07-MultipleArrays/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] array2 = new int[4][4];
+        System.out.println(Arrays.toString(array2));
+        System.out.println("array2.length = " + array2.length);
+
+    }
+}
