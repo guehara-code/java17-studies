9abcda52b633abaf09af65cd6aa890b0618cc25a
Gilberto <gil_uehara@yahoo.com.br>
2023-12-04 08:30:23 -0300

    more studies

--- _09-05-VarArgs/src/Main.java
+++ _09-05-VarArgs/src/Main.java
@@ -7,7 +7,17 @@ public class Main {
         String[] splitStrings = "Hello World again".split(" ");
         printText(splitStrings);
 
-        
+        System.out.println("_".repeat(20));
+        printText("Hello");
+
+        System.out.println("_".repeat(20));
+        printText("Hello", "World", "again");
+
+        System.out.println("_".repeat(20));
+        printText();
+
+        String[] sArray = {"first", "second", "third", "fourth", "fifth"};
+        System.out.println(String.join(",", sArray));
     }
 
     private static void printText(String... textList) {
