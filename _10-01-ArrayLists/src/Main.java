2d1e74af1467ade3f977da19769301e29e6c2adb
Gilberto <gil_uehara@yahoo.com.br>
2023-12-05 08:36:02 -0300

    more studies

--- _10-01-ArrayLists/src/Main.java
+++ _10-01-ArrayLists/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 record GroceryItem(String name, String type, int count) {
@@ -11,11 +12,19 @@ record GroceryItem(String name, String type, int count) {
 public class Main {
 
     public static void main(String[] args) {
-        Object[] groceryArray = new Object[3];
+        GroceryItem[] groceryArray = new GroceryItem[3];
         groceryArray[0] = new GroceryItem("milk");
         groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
-        groceryArray[2] = "5 oranges";
+        groceryArray[2] = new GroceryItem("5 oranges", "PRODUCE", 5);
         System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        
     }
 
 }
