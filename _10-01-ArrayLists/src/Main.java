1d28bde839d8b0fe37e89e741fb457086ab45a90
Gilberto <gil_uehara@yahoo.com.br>
2023-12-05 07:09:57 -0300

    more studies

---
+++ _10-01-ArrayLists/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count) {
+
+    public GroceryItem(String name) {
+        this(name, "DAIRY", 1);
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Object[] groceryArray = new Object[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = "5 oranges";
+        System.out.println(Arrays.toString(groceryArray));
+    }
+
+}
