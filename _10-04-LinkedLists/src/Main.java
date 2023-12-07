8b01d4f02a726dbc0cc08b4dcfde1e0da420b386
Gilberto <gil_uehara@yahoo.com.br>
2023-12-07 09:06:02 -0300

    more studies

--- _10-04-LinkedLists/src/Main.java
+++ _10-04-LinkedLists/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
 
@@ -18,6 +19,8 @@ public class Main {
 //        System.out.println(placesToVisit);
 
         gettingElements(placesToVisit);
+
+        printItinerary3(placesToVisit);
     }
 
     private static void addMoreElements(LinkedList<String> list) {
@@ -81,6 +84,44 @@ public class Main {
         System.out.println("Element from peek() = " + list.peek());
         System.out.println("Element from peekFirst() = " + list.peekFirst());
         System.out.println("Element from peekLast() = " + list.peekLast());
+
+    }
+
+    public static void printItinerary(LinkedList<String> list) {
+
+        System.out.println("Trip starts at " + list.getFirst());
+        for (int i = 1; i < list.size(); i++) {
+            System.out.println("--> From: " + list.get(i - 1) + " to " + list.get(i));
+        }
+
+        System.out.println("Trip ends at " + list.getLast());
+
+    }
+
+    public static void printItinerary2(LinkedList<String> list) {
+
+        System.out.println("Trip starts at " + list.getFirst());
+        String previousTown = list.getFirst();
+        for (String town : list) {
+            System.out.println("--> From: " + previousTown + " to " + town);
+            previousTown = town;
+        }
+
+        System.out.println("Trip ends at " + list.getLast());
+    }
+
+    public static void printItinerary3(LinkedList<String> list) {
+
+        System.out.println("Trip starts at " + list.getFirst());
+        String previousTown = list.getFirst();
+        ListIterator<String> iterator = list.listIterator(1);
+        while (iterator.hasNext()) {
+            var town = iterator.next();
+            System.out.println("--> From: " + previousTown + " to " + town);
+            previousTown = town;
+        }
+
+        System.out.println("Trip ends at " + list.getLast());
 
     }
 
