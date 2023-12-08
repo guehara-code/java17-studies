c699f21d124f1440c3f74fbb5fc993e7effdd4bd
Gilberto <gil_uehara@yahoo.com.br>
2023-12-08 09:11:17 -0300

    more studies

--- _10-05-LinkedListChallenge/src/Main.java
+++ _10-05-LinkedListChallenge/src/Main.java
@@ -18,6 +18,14 @@ public class Main {
 
         Place adelaide = new Place("Adelaide", 1374);
         addPlace(placesToVisit, adelaide);
+        addPlace(placesToVisit, new Place("adelaide", 1374));
+        addPlace(placesToVisit, new Place("Brisbane", 917));
+        addPlace(placesToVisit, new Place("Perth", 3923));
+        addPlace(placesToVisit, new Place("Alice Springs", 2771));
+        addPlace(placesToVisit, new Place("Darwin", 3972));
+        addPlace(placesToVisit, new Place("Melbourne", 877));
+
+        placesToVisit.addFirst(new Place("Sydney", 0));
         System.out.println(placesToVisit);
 
     }
@@ -26,6 +34,20 @@ public class Main {
         if (list.contains(place)) {
             System.out.println("Found duplicate: " + place);
             return;
+        }
+        for(Place p : list) {
+            if (p.name().equalsIgnoreCase(place.name())) {
+                System.out.println("Found duplicate: " + place);
+                return;
+            }
+        }
+        int matchedIndex = 0;
+        for (var listPlace : list) {
+            if (place.distance() < listPlace.distance()) {
+                list.add(matchedIndex, place);
+                return;
+            }
+            matchedIndex++;
         }
         list.add(place);
     }
