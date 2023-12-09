505cf9034917428535c19b3f1e2905b3193c6f42
Gilberto <gil_uehara@yahoo.com.br>
2023-12-09 09:13:07 -0300

    more studies

--- _10-06-Autoboxing/src/Main.java
+++ _10-06-Autoboxing/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -15,20 +18,52 @@ public class Main {
         Double resultBoxed = getLiteralDoublePrimitive();
         double resultUnboxed = getDoubleObject();
 
+        Integer[] wrapperArray = new Integer[5];
+        wrapperArray[0] = 50;
+        System.out.println(Arrays.toString(wrapperArray));
 
-     }
+        System.out.println(wrapperArray[0].getClass().getName());
 
-     private static Double getDoubleObject() {
+        Character[] characterArray = {'a', 'b', 'c', 'd'};
+        System.out.println(Arrays.toString(characterArray));
+
+        var outList = getList(1, 2, 3, 4, 5);
+        System.out.println(outList);
+
+    }
+
+    private static ArrayList<Integer> getList(Integer... varargs) {
+
+        ArrayList<Integer> aList = new ArrayList<>();
+        for (int i: varargs) {
+            aList.add(i);
+        }
+        return aList;
+    }
+
+    private static int returnAnInt(Integer i) {
+
+        return i;
+
+    }
+
+    private static Integer returnAnInteger(int i) {
+
+        return i;
+
+    }
+
+    private static Double getDoubleObject() {
 
         return Double.valueOf(100.00);
 
-     }
+    }
 
-     private static double getLiteralDoublePrimitive() {
+    private static double getLiteralDoublePrimitive() {
 
         return 100.00;
 
-     }
+    }
 
 }
 
