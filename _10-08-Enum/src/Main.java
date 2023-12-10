08f6abca773166f3f94f236722ed70df54260f1f
Gilberto <gil_uehara@yahoo.com.br>
2023-12-10 17:27:03 -0300

    more studies

--- _10-08-Enum/src/Main.java
+++ _10-08-Enum/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,5 +7,25 @@ public class Main {
         DayOfTheWeek weekDay = DayOfTheWeek.TUES;
         System.out.println(weekDay);
 
+
+        for (int i = 0; i < 10; i++ ){
+
+            weekDay = getRandomDay();
+            System.out.printf("Name id %s, Ordinal Value = %d%n",
+                    weekDay.name(), weekDay.ordinal());
+            if (weekDay == DayOfTheWeek.FRI) {
+                System.out.println(("Found a Friday!!!"));
+            }
+        }
+
     }
+
+    public static DayOfTheWeek getRandomDay() {
+
+        int randomInteger = new Random().nextInt(7);
+        var allDays = DayOfTheWeek.values();
+
+        return allDays[randomInteger];
+    }
+
 }
