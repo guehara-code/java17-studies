40ccbea8a64cba6d6a6b916be855bedceb7afe30
Gilberto <gil_uehara@yahoo.com.br>
2023-12-16 09:06:54 -0300

    more studies

---
+++ _11-04-InterfaceChallenge/src/Building.java
@@ -0,0 +1,33 @@
+enum UsageType {ENTERTAINEMENT, GOVERNMENT, RESIDENTIAL, SPORTS}
+public class Building implements Mappable {
+
+    private String name;
+    private UsageType usage;
+
+    public Building(String name, UsageType usage) {
+        this.name = name;
+        this.usage = usage;
+    }
+
+
+    @Override
+    public String getLabel() {
+        return name + " (" + usage + ")";
+    }
+
+    @Override
+    public Geometry getShape() {
+        return Geometry.POINT;
+    }
+
+    @Override
+    public String getMarker() {
+        return switch (usage) {
+            case ENTERTAINEMENT -> Color.GREEN + " " + PointMarker.TRIANGLE;
+            case GOVERNMENT -> Color.RED + " " + PointMarker.STAR;
+            case RESIDENTIAL -> Color.BLUE + " " + PointMarker.SQUARE;
+            case SPORTS -> Color.ORANGE + " " + PointMarker.PUSH_PIN;
+            default -> Color.BLUE + " " + PointMarker.CIRCLE;
+        };
+    }
+}
