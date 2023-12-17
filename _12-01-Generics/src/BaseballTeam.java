06b3b5d9ff4227006aa5b1715064606dd0f340be
Gilberto <gil_uehara@yahoo.com.br>
2023-12-17 09:02:59 -0300

    more studies

---
+++ _12-01-Generics/src/BaseballTeam.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballTeam {
+
+    private String teamName;
+    private List<BaseballPlayer> teamMembers = new ArrayList<>();
+    private int totalWins = 0;
+    private int totalLosses = 0;
+    private int totalTies = 0;
+
+}
---
+++ _12-01-Generics/src/Main.java
@@ -0,0 +1,9 @@
+record BaseballPlayer(String name, String position) {}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
