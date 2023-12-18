c13735c069ed106490e41abed60e75513699c0f9
Gilberto <gil_uehara@yahoo.com.br>
2023-12-18 16:59:22 -0300

    more studies

--- _12-03-Comparing/src/Main.java
+++ _12-03-Comparing/src/Main.java
@@ -27,7 +27,31 @@ public class Main {
         System.out.println("B:" + (int)'B' + " " + "b:" + (int)'b');
         System.out.println("P:" + (int)'P' + " " + "p:" + (int)'p');
 
+        Student tim = new Student("Tim");
+        Student[] students = {new Student("Zach"), new Student("Tim"), new Student("Ann")};
+
+        Arrays.sort(students);
+        System.out.println(Arrays.toString(students));
+
+//        System.out.println("result = " + tim.compareTo("Mary"));
     }
 }
 
+class Student implements Comparable {
 
+    private String name;
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Student other = (Student) o;
+        return name.compareTo(other.name);
+    }
+}
