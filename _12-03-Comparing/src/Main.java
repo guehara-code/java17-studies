8b14b903d1fb33d917b9c6110a42f305b34cf4c4
Gilberto <gil_uehara@yahoo.com.br>
2023-12-19 08:56:01 -0300

    more studies

--- _12-03-Comparing/src/Main.java
+++ _12-03-Comparing/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
 
@@ -39,19 +40,28 @@ public class Main {
 
 class Student implements Comparable<Student> {
 
+
+    private static int LAST_ID = 1000;
+    private static Random random = new Random();
+
     private String name;
+    private int id;
+    protected double gpa;
+
     public Student(String name) {
         this.name = name;
+        id = LAST_ID++;
+        gpa = random.nextDouble(1.0, 4.0);
     }
 
     @Override
     public String toString() {
-        return name;
+        return "%d - %s (%.2f)".formatted(id, name, gpa);
     }
 
     @Override
     public int compareTo(Student o) {
-        return name.compareTo(o.name);
+        return Integer.valueOf(id).compareTo(Integer.valueOf(o.id));
     }
 
 //    @Override
