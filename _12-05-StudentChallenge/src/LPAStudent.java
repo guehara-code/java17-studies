ff906e175ee503fa217f6f8fedfc9ae3508602ea
Gilberto <gil_uehara@yahoo.com.br>
2023-12-20 17:27:33 -0300

    more studies

--- _12-05-StudentChallenge/src/LPAStudent.java
+++ _12-05-StudentChallenge/src/LPAStudent.java
@@ -11,7 +11,17 @@ public class LPAStudent extends Student {
         return "%s %8.1f%%".formatted(super.toString(), percentComplete);
     }
 
+
+
     public double getPercentComplete() {
         return percentComplete;
+    }
+
+    @Override
+    public boolean matchFieldValue(String fieldName, String value) {
+        if (fieldName.equalsIgnoreCase("percentComplete")) {
+            return percentComplete <= Integer.parseInt(value);
+        }
+        return super.matchFieldValue(fieldName, value);
     }
 }
--- _12-05-StudentChallenge/src/Main.java
+++ _12-05-StudentChallenge/src/Main.java
@@ -6,13 +6,24 @@ public class Main {
     public static void main(String[] args) {
 
         QueryList<LPAStudent> queryList = new QueryList<>();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 25; i++) {
             queryList.add(new LPAStudent());
         }
 
         System.out.println("Ordered");
         queryList.sort(Comparator.naturalOrder());
         printList(queryList);
+
+        System.out.println("Matches");
+        var matches = queryList
+                .getMatches("PercentComplete", "50")
+                .getMatches("Course", "Python");
+        matches.sort(new LPAStudentComparator());
+        printList(matches);
+
+        System.out.println("Ordered");
+        matches.sort(null);
+        printList(matches);
     }
 
     public static void printList(List<?> students) {
