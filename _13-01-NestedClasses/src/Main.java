ea994e760bdf09a19dec873d30ccb4cb1f5ce0a1
Gilberto <gil_uehara@yahoo.com.br>
2023-12-21 14:05:07 -0300

    more studies

--- _13-01-NestedClasses/src/Main.java
+++ _13-01-NestedClasses/src/Main.java
@@ -22,5 +22,23 @@ public class Main {
             System.out.println(e);
         }
 
+        System.out.println("Store Members");
+
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+                new StoreEmployee(10015, "Meg", 2019, "Target"),
+                new StoreEmployee(10515, "Joe", 2021, "Walmart"),
+                new StoreEmployee(10105, "Tom", 2029, "Macys"),
+                new StoreEmployee(10215, "Marty", 2018, "Walmart"),
+                new StoreEmployee(10322, "Bud", 2016, "Target")
+        ));
+
+        var genericEmployee = new StoreEmployee();
+        var comparator = genericEmployee.new StoreComparator<>();
+        storeEmployees.sort(comparator);
+
+        for (StoreEmployee e : storeEmployees) {
+            System.out.println(e);
+        }
+
     }
 }
