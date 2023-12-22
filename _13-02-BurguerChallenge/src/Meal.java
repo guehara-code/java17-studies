67811f19399543bdd184b603b05b3dfb92298666
Gilberto <gil_uehara@yahoo.com.br>
2023-12-22 20:07:26 -0300

    more studies

--- _13-02-BurguerChallenge/src/Meal.java
+++ _13-02-BurguerChallenge/src/Meal.java
@@ -4,8 +4,19 @@ public class Meal {
     private Item burger;
     private Item drink;
     private Item side;
+    private double conversionRate;
 
     public Meal() {
+        this(1);
+    }
+
+    public double getTotal() {
+        double total = burger.price + drink.price + side.price;
+        return Item.getPrice(total, conversionRate);
+    }
+
+    public Meal(double conversionRate) {
+        this.conversionRate = conversionRate;
         burger = new Item("regular", "burger");
         drink = new Item("coke", "drink" , 1.5);
         System.out.println(drink.name);
@@ -14,7 +25,7 @@ public class Meal {
 
     @Override
     public String toString() {
-        return "%s%n%s%n%s%n".formatted(burger, drink, side);
+        return "%s%n%s%n%s%n%26s$%.2f".formatted(burger, drink, side, "Total Due: ", getTotal());
     }
 
     private class Item {
@@ -35,7 +46,11 @@ public class Meal {
 
         @Override
         public String toString() {
-            return "%10s%15s $%.2f".formatted(type, name, price);
+            return "%10s%15s $%.2f".formatted(type, name, getPrice(price, conversionRate));
+        }
+
+        private static double getPrice(double price, double rate) {
+            return price*rate;
         }
     }
 }
