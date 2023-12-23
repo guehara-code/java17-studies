9bb8f5b475016bddc3b5bdc058f36890e7a9d556
Gilberto <gil_uehara@yahoo.com.br>
2023-12-23 07:07:02 -0300

    more studies

--- _13-02-BurguerChallenge/src/Meal.java
+++ _13-02-BurguerChallenge/src/Meal.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal {
 
     private double price = 5.0;
@@ -28,6 +31,10 @@ public class Meal {
         return "%s%n%s%n%s%n%26s$%.2f".formatted(burger, drink, side, "Total Due: ", getTotal());
     }
 
+    public void addToppings(String... selectedToppings) {
+        burger.addToppings(selectedToppings);
+    }
+
     private class Item {
 
         private String name;
@@ -56,12 +63,53 @@ public class Meal {
 
     private class Burger extends Item {
 
+        private enum Extra {AVOCADO, BACON, CHEESE, KETCHUP, MAYO, MUSTARD, PICKLES;
+
+            private double getPrice() {
+                return switch (this) {
+                    case AVOCADO -> 1.0;
+                    case BACON, CHEESE -> 1.5;
+                    default -> 0;
+                };
+            };
+
+        }
+        private List<Item> toppings = new ArrayList<>();
+
         Burger(String name) {
             super(name, "burger", 5.0);
         }
 
         public double getPrice() {
-            return super.price;
+
+            double total = super.price;
+            for (Item topping : toppings) {
+                total += topping.price;
+            }
+            return total;
+        }
+
+        private void addToppings(String... selectedToppings) {
+
+            for (String selectedTopping : selectedToppings) {
+                try {
+                    Extra topping = Extra.valueOf(selectedTopping.toUpperCase());
+                    toppings.add(new Item(topping.name(), "TOPPING", topping.getPrice()));
+                } catch (IllegalArgumentException ie) {
+                    System.out.println("No topping found for " + selectedTopping);
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+
+            StringBuilder itemized = new StringBuilder(super.toString());
+            for(Item topping : toppings) {
+                itemized.append("\n");
+                itemized.append(topping);
+            }
+            return itemized.toString();
         }
 
     }
