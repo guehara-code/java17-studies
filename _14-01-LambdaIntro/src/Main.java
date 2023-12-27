e06e8d60954ba2353755c1c84a0ccb23086354ce
Gilberto <gil_uehara@yahoo.com.br>
2023-12-27 09:07:25 -0300

    more studies

---
+++ _14-01-LambdaIntro/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    record Person(String firstName, String lastName) {
+
+        public String toString() {
+            return firstName + " " + lastName;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        List<Person> people = new ArrayList<>(Arrays.asList(
+                new Main.Person("Lucy", "Van Pelt"),
+                new Person("Sally", "Brown"),
+                new Person("Linus", "Van Pelt"),
+                new Person("Peppermint", "Patty"),
+                new Person("Charlie", "Brown")
+        ));
+
+        // Using anonymous class
+        var comparatorLastName = new Comparator<Person>() {
+
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.lastName().compareTo(o2.lastName());
+            }
+        };
+
+        people.sort((o1, o2) -> o1.lastName().compareTo(o2.lastName()));
+        System.out.println(people);
+    }
+}
