25c86d9b6214b14485b456916464d413133b895d
Gilberto <gil_uehara@yahoo.com.br>
2023-12-31 09:20:54 -0300

    more studies

---
+++ _14-03-MiniLambdaChallenges/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.function.Consumer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Consumer<String> printWorks = new Consumer<String>() {
+            @Override
+            public void accept(String sentence) {
+                String[] parts = sentence.split(" ");
+                for (String part : parts) {
+                    System.out.println(part);
+                }
+            }
+        };
+    }
+    
+    public static String everySecondChar(String source) {
+        
+        StringBuilder returnVal = new StringBuilder();
+        for (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
+                returnVal.append(source.charAt(i));
+            }
+        }
+        return returnVal.toString();
+    }
+}
