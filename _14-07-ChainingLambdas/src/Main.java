6af414d9efa206f12f835cb953620dbf94b961f9
Gilberto <gil_uehara@yahoo.com.br>
2024-01-04 08:55:22 -0300

    more studies

---
+++ _14-07-ChainingLambdas/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Tim";
+        Function<String, String> uCase = String::toUpperCase;
+        System.out.println(uCase.apply(name));
+
+        Function<String, String> lastName = s -> s.concat(" Scott");
+        Function<String, String> uCaseLastName = uCase.andThen(lastName);
+        System.out.println(uCaseLastName.apply(name));
+
+        uCaseLastName = uCase.compose(lastName);
+        System.out.println(uCaseLastName.apply(name));
+
+        Function<String, String[]> f0 = uCase
+                .andThen(s -> s.concat(" Scott"))
+                .andThen(s -> s.split(" "));
+        System.out.println(Arrays.toString(f0.apply(name)));
+    }
+}
