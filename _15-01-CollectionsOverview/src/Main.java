16845823dd0b53d2252d7617fc77177f95ce3a22
Gilberto <gil_uehara@yahoo.com.br>
2024-01-05 08:49:35 -0300

    more studies

--- _15-01-CollectionsOverview/src/Main.java
+++ _15-01-CollectionsOverview/src/Main.java
@@ -1,15 +1,22 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
+        Collection<String> list = new HashSet<>();
 
         String[] names = {"Anna", "Bob", "Carol", "David", "Edna"};
         list.addAll(Arrays.asList(names));
         System.out.println(list);
+
+        list.add("Fred");
+        list.addAll(Arrays.asList("George", "Gary", "Grace"));
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
+
+        list.removeIf(s -> s.charAt(0) == 'G');
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
     }
 }
