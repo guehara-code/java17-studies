ebdcec2516b14b1b9f94524b4c6c0d579228c0f6
Gilberto <gil_uehara@yahoo.com.br>
2024-01-05 09:19:38 -0300

    more studies

--- _15-02-CollectionsMethods/src/Main.java
+++ _15-02-CollectionsMethods/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -6,5 +7,14 @@ public class Main {
 
         List<Card> deck = Card.getStandardDeck();
         Card.printDeck(deck);
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
+        Arrays Array;
+        Arrays.fill(cardArray, aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
+
+        
+
     }
 }
