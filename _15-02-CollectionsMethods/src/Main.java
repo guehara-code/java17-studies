56cb63509aef54c104a0cea3d5e03dc52871500f
Gilberto <gil_uehara@yahoo.com.br>
2024-01-06 09:04:57 -0300

    more studies

--- _15-02-CollectionsMethods/src/Main.java
+++ _15-02-CollectionsMethods/src/Main.java
@@ -71,7 +71,31 @@ public class Main {
         Card tenOfHearts = Card.getNumericCard(Card.Suit.HEART, 10);
         int foundIndex = Collections.binarySearch(deck, tenOfHearts, sortingAlgorithm);
         System.out.println("foundIndex = " + foundIndex);
+        System.out.println("foundIndex = " + deck.indexOf(tenOfHearts));
         System.out.println(deck.get(foundIndex));
+
+        Card tenOfClubs = Card.getNumericCard(Card.Suit.CLUB, 10);
+        Collections.replaceAll(deck, tenOfClubs, tenOfHearts);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        Collections.replaceAll(deck, tenOfHearts, tenOfClubs);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        if (Collections.replaceAll(deck, tenOfHearts, tenOfClubs)) {
+            System.out.println("Tens of hearts replaced with tens of clubs");
+        } else {
+            System.out.println("No tens of hearts found in the list");
+        }
+
+        System.out.println("Ten of Clubs Cards = " +
+                Collections.frequency(deck, tenOfClubs));
+
+        System.out.println("Best Card = " + Collections.max(deck, sortingAlgorithm));
+        System.out.println("Worst Card = " + Collections.min(deck, sortingAlgorithm));
+
+        var sortBySuit = Comparator.comparing(Card::suit).thenComparing(Card::rank);
+        deck.sort(sortBySuit);
+        Card.printDeck(deck, "Sorted by Suit, Rank", 4);
 
     }
 }
