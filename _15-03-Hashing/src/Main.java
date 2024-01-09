90e0c7be989d09d076881e4f0ad797e4973756d3
Gilberto <gil_uehara@yahoo.com.br>
2024-01-09 20:13:57 -0300

    more studies

--- _15-03-Hashing/src/Main.java
+++ _15-03-Hashing/src/Main.java
@@ -29,5 +29,20 @@ public class Main {
             }
             System.out.println(" ");
         }
+
+        PlayingCard aceHearts = new PlayingCard("Hearts", "Ace");
+        PlayingCard kingClubs = new PlayingCard("Clubs", "King");
+        PlayingCard queenSpades = new PlayingCard("Spades", "Queen");
+
+        List<PlayingCard> cards = Arrays.asList(aceHearts, kingClubs, queenSpades);
+        cards.forEach(s -> System.out.println(s + ": " + s.hashCode()));
+
+        Set<PlayingCard> deck = new HashSet<>();
+        for (PlayingCard c : cards) {
+            if (!deck.add(c)) {
+                System.out.println("Found a duplicate for " + c);
+            }
+        }
+        System.out.println(deck);
     }
 }
