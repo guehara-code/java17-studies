b2c956f068977be13076f416c52ce1372554da8f
Gilberto <gil_uehara@yahoo.com.br>
2024-01-09 09:05:44 -0300

    more studies

---
+++ _15-03-Hashing/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String aText = "Hello";
+        String bText = "Hello";
+        String cText = String.join("l", "He", "lo");
+        String dText = "He".concat("llo");
+        String eText = "hello";
+
+        List<String> hellos = Arrays.asList(aText, bText, cText, dText, eText);
+
+        hellos.forEach(s -> System.out.println(s + ": " + s.hashCode()));
+    }
+}
