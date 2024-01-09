6c080dfeb5f82807730f6633e53464ec080ea834
Gilberto <gil_uehara@yahoo.com.br>
2024-01-09 20:26:11 -0300

    more studies

--- _15-03-Hashing/src/PlayingCard.java
+++ _15-03-Hashing/src/PlayingCard.java
@@ -14,5 +14,23 @@ public class PlayingCard {
     public String toString() {
         return face + " of " + suit;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlayingCard that = (PlayingCard) o;
+
+        if (!suit.equals(that.suit)) return false;
+        return face.equals(that.face);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit.hashCode();
+        result = 31 * result + face.hashCode();
+        return result;
+    }
 }
 
