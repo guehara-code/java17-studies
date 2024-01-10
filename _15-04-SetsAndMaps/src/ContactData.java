ce87359514b38397fe351d355d7175eea89473b4
Gilberto <gil_uehara@yahoo.com.br>
2024-01-10 09:03:27 -0300

    more studies

---
+++ _15-04-SetsAndMaps/src/ContactData.java
@@ -0,0 +1,22 @@
+public class ContactData {
+
+    private static final String phoneData = """
+            Charlie Brown, 3334445555
+            Maid Marion, 1234567890
+            Mickey Mouse, 9998887777
+            Minnie Mouse, 4567805666
+            Robin Hood, 5647893000
+            Robin Hood, 7899028222
+            Lucy Van Pelt, 5642086852
+            Mickey Mouse, 9998887777
+            """;
+
+    private static final String emailData = """
+            Mickey Mouse, mckmouse@gmail.com
+            Mickey Mouse, michy1@aws.com
+            Minnie Mouse, minnie@verizon.net
+            Robin Hood, rhood@gmail.com
+            Linus Van Pelt, lvpelt2015@gmail.com
+            Daffy Duck, daffy@google.com
+            """;
+}
