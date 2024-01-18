f91b11097fb61377b98bc7a135fcae1abc760432
Gilberto <gil_uehara@yahoo.com.br>
2024-01-18 09:24:56 -0300

    more studies

--- _15-06-TreeSetChallenge/src/BookingAgent.java
+++ _15-06-TreeSetChallenge/src/BookingAgent.java
@@ -7,5 +7,22 @@ public class BookingAgent {
         Theatre rodgerNYC = new Theatre("Richard Rodgers", rows, totalSeats);
 
         rodgerNYC.printSeatMap();
+
+        bookSeat(rodgerNYC, 'A', 3);
+        bookSeat(rodgerNYC, 'A', 3);
+        bookSeat(rodgerNYC, 'B', 1);
+        bookSeat(rodgerNYC, 'B', 11);
+        bookSeat(rodgerNYC, 'M', 1);
+    }
+
+    private static void bookSeat(Theatre theatre, char row, int seatNo) {
+
+        String seat = theatre.reserveSeat(row, seatNo);
+        if (seat != null) {
+            System.out.println("Congratulations! Your reserved seat is " + seat);
+            theatre.printSeatMap();
+        } else {
+            System.out.println("Sorry! Unable to reserve " + row + seatNo);
+        }
     }
 }
