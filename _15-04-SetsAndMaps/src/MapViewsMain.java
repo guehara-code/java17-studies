8517557d297d70c66c66fd62e172741e25a85131
Gilberto <gil_uehara@yahoo.com.br>
2024-01-26 09:09:13 -0300

    more studies

--- _15-04-SetsAndMaps/src/MapViewsMain.java
+++ _15-04-SetsAndMaps/src/MapViewsMain.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class MapViewsMain {
 
@@ -12,5 +13,16 @@ public class MapViewsMain {
 
         Set<String> keysView = contacts.keySet();
         System.out.println(keysView);
+
+        Set<String> copyOfKeys = new TreeSet<>(contacts.keySet());
+        System.out.println(copyOfKeys);
+
+        if (contacts.containsKey("Linus Van Pelt")) {
+            System.out.println("Linus and I go way back, so of course I have info");
+        }
+
+        keysView.remove("Daffy Duck");
+        System.out.println(keysView);
+        contacts.forEach((k, v) -> System.out.println(v));
     }
 }
