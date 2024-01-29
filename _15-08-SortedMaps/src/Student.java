db14e9bb0026306c954ae2985b2ab5ce6a1758d7
Gilberto <gil_uehara@yahoo.com.br>
2024-01-29 09:12:17 -0300

    more studies

---
+++ _15-08-SortedMaps/src/Student.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+import java.util.Locale;
+
+record Course(String courseId, String name, String subject) {
+
+}
+
+record Purchase(String courseId, int studentId, double price, int yr, int dayOfYear) {
+
+    public LocalDate purchaseDate() {
+        return LocalDate.ofYearDay(yr, dayOfYear);
+    }
+}
+public class Student {
+}
