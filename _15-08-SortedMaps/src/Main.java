d78277a57ec59994bba87670324a160ea23858c7
Gilberto <gil_uehara@yahoo.com.br>
2024-01-29 14:07:31 -0300

    more studies

--- _15-08-SortedMaps/src/Main.java
+++ _15-08-SortedMaps/src/Main.java
@@ -1,6 +1,30 @@
+import java.time.LocalDate;
+import java.util.*;
+
 public class Main {
 
+    private static Map<String, Purchase> purchases = new LinkedHashMap<>();
+    private static NavigableMap<String, Student> students = new TreeMap<>();
     public static void main(String[] args) {
 
+
+    }
+
+    private static void addPurchase(String name, Course course, double price) {
+
+        Student existStudent = students.get(name);
+        if (existStudent == null) {
+            existStudent = new Student(name, course);
+            students.put(name, existStudent);
+        } else {
+            existStudent.addCourse(course);
+        }
+
+        int day = purchases.size() + 1;
+        String key = course.courseId() + "-" + existStudent.getId();
+        int year = LocalDate.now().getYear();
+        Purchase purchase = new Purchase(course.courseId(),
+                existStudent.getId(), price, year, day);
+        purchases.put(key, purchase);
     }
 }
