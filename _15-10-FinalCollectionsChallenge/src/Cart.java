558ddaa25ed52c8c5c691a980c15986f9f99bc21
Gilberto <gil_uehara@yahoo.com.br>
2024-02-03 20:23:53 -0300

    more studies

--- _15-10-FinalCollectionsChallenge/src/Cart.java
+++ _15-10-FinalCollectionsChallenge/src/Cart.java
@@ -32,5 +32,53 @@ public class Cart {
         return cartDate;
     }
 
+    public void addItem(InventoryItem item, int qty) {
 
+        products.merge(item.getProduct().sku(), qty, Integer::sum);
+
+        if (!item.reserveItem(qty)) {
+            System.out.println("Ouch!, something went wrong, could not add item");
+        }
+    }
+
+    public void removeItem(InventoryItem item, int qty) {
+
+        int current = products.get(item.getProduct().sku());
+        if (current <= qty) {
+            qty = current;
+            products.remove(item.getProduct().sku());
+            System.out.printf("Item [%s] removed from basket%n", item.getProduct().name());
+        } else {
+            products.merge(item.getProduct().sku(), qty,
+                    (oldVal, newVal) -> oldVal - newVal);
+            System.out.printf("%d [%s]s removed%n", qty, item.getProduct().name());
+        }
+        item.releaseItem(qty);
+    }
+
+    public void printSalesSlip(Map<String, InventoryItem> inventory) {
+
+        double total = 0;
+        System.out.println("----------------------------------------");
+        System.out.println("Thank you for your sale: ");
+        for (var cartItem : products.entrySet()) {
+            var item = inventory.get(cartItem.getKey());
+            int qty = cartItem.getValue();
+            double itemizedPrice = (item.getPrice() * qty);
+            total += itemizedPrice;
+            System.out.printf("\t%s %-10s (%d)@ $%.2f = $%.2f%n",
+                    cartItem.getKey(), item.getProduct().name(), qty, item.getPrice(), itemizedPrice);
+        }
+        System.out.printf("Total Sale: $%.2f%n", total);
+        System.out.println("----------------------------------------");
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "id=" + id +
+                ", cartDate=" + cartDate +
+                ", products=" + products +
+                '}';
+    }
 }
