75ffa12ae32235847711495b3e68ff2a4906d9e1
Gilberto <gil_uehara@yahoo.com.br>
2024-02-04 09:04:52 -0300

    more studies

--- _15-10-FinalCollectionsChallenge/src/Store.java
+++ _15-10-FinalCollectionsChallenge/src/Store.java
@@ -20,4 +20,41 @@ public class Store {
 
         return false;
     }
+
+    private void abandonCarts() {
+
+    }
+
+    private void listProductsByCategory() {
+
+    }
+
+    private void stockStore() {
+
+        inventory = new HashMap<>();
+        List<Product> products = new ArrayList<>(List.of(
+                new Product("A100","apple","local",Category.PRODUCE),
+                new Product("B100","banana","local",Category.PRODUCE),
+                new Product("P100","pear","local",Category.PRODUCE),
+                new Product("L103","lemon","local",Category.PRODUCE),
+                new Product("M201","milk","farm",Category.DAIRY),
+                new Product("Y001","yogurt","farm",Category.DAIRY),
+                new Product("C333","cheese","farm",Category.DAIRY),
+                new Product("R777","rice chex","Nabisco",Category.CEREAL),
+                new Product("G111","granola","Nat Valley",Category.CEREAL),
+                new Product("BB11","ground beef","butcher",Category.MEAT),
+                new Product("CC11","chicken","butcher",Category.MEAT),
+                new Product("BC11","bacon","butcher",Category.MEAT),
+                new Product("BC77","coke","coca cola",Category.BEVERAGE),
+                new Product("BC88","coffee","value",Category.BEVERAGE),
+                new Product("BC99","tea","herbal",Category.BEVERAGE)
+        ));
+        products.forEach(p -> inventory.put(p.sku(), new InventoryItem(p,
+                random.nextDouble(0, 1.25), 1000, 5)));
+    }
+
+    private void stockAisles() {
+
+    }
+
 }
