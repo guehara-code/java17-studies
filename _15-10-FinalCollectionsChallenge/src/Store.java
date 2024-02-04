164b4d02808d0baad9fd9095db6b6a438fadf669
Gilberto <gil_uehara@yahoo.com.br>
2024-02-04 08:58:45 -0300

    more studies

--- _15-10-FinalCollectionsChallenge/src/Store.java
+++ _15-10-FinalCollectionsChallenge/src/Store.java
@@ -1,7 +1,23 @@
+import java.util.*;
+
 public class Store {
+
+    private static Random random = new Random();
+    private Map<String, InventoryItem> inventory;
+    private NavigableSet<Cart> carts = new TreeSet<>(Comparator.comparing(Cart::getId));
+    private Map<Category, Map<String, InventoryItem>> asleInventory;
 
     public static void main(String[] args) {
 
 
+    }
+
+    private void manageStoreCarts() {
+
+    }
+
+    private boolean checkOUtCart(Cart cart) {
+
+        return false;
     }
 }
