23971f200c73c9bcef0c79d7d4d388181330c7c4
Gilberto <gil_uehara@yahoo.com.br>
2024-02-07 20:30:12 -0300

    more studies

--- _16-02-ImmutableClasses/src/dev/lpa/Main.java
+++ _16-02-ImmutableClasses/src/dev/lpa/Main.java
@@ -4,20 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Person jane = new Person();
-        jane.setName("Jane");
-        Person jim = new Person();
-        jim.setName("Jin");
-        Person joe = new Person();
-        joe.setName("Joe");
-        Person john = new Person();
-        john.setName("John");
-        john.setDob("05/05/1900");
-        john.setKids(new Person[]{jane, jim, john});
+//        Person jane = new Person();
+//        jane.setName("Jane");
+//        Person jim = new Person();
+//        jim.setName("Jin");
+//        Person joe = new Person();
+//        joe.setName("Joe");
+//        Person john = new Person();
+//        john.setName("John");
+//        john.setDob("05/05/1900");
+//        john.setKids(new Person[]{jane, jim, john});
+//        System.out.println(john);
+//
+//        john.setName("Jacob");
+//        john.setKids(new Person[]{new Person(), new Person()});
+//        System.out.println(john);
+
+        Person jane = new Person("Jane", "01/01/1930");
+        Person jim = new Person("Jim", "02/02/1932");
+        Person joe = new Person("Joe", "03/03/1934");
+
+        Person[] johnsKids = {jane, jim, joe};
+        Person john = new Person("John", "05/02/1900", johnsKids);
+
         System.out.println(john);
 
-        john.setName("Jacob");
-        john.setKids(new Person[]{new Person(), new Person()});
+        john.setKids(new Person[]{new Person("Ann", "04/04/1930")});
         System.out.println(john);
     }
 }
