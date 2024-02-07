9dd982863a73852e2e152c5789d0ef40e8bc679f
Gilberto <gil_uehara@yahoo.com.br>
2024-02-07 09:07:59 -0300

    more studies

---
+++ _16-01-FinalExplored/src/external/util/Logger.java
@@ -0,0 +1,14 @@
+package external.util;
+
+
+import java.time.LocalDateTime;
+
+public class Logger {
+
+    public static void logToConsole(CharSequence message) {
+
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.printf("%1$tD %1$tT : %2$s%n", dt, message);
+
+    }
+}
