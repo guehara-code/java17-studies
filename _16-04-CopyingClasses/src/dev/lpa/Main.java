b68b781cecae9bbf4d2cee76b8b1a25e0106cacd
Gilberto <gil_uehara@yahoo.com.br>
2024-02-09 09:31:36 -0300

    more studies

--- _16-04-CopyingClasses/src/dev/lpa/Main.java
+++ _16-04-CopyingClasses/src/dev/lpa/Main.java
@@ -3,20 +3,32 @@ package dev.lpa;
 
 import java.util.Arrays;
 
-record Person (String name, String dob) {}
+record Person (String name, String dob, Person[] kids) {
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", kids=" + Arrays.toString(kids) +
+                '}';
+    }
+}
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Person joe = new Person("Joe", "01/01/1961");
-        Person jim = new Person("Jim", "02/02/1961");
-        Person jack = new Person("Jack", "03/03/1961");
-        Person jane = new Person("Jane", "04/04/1961");
-        Person jill = new Person("Jill", "05/05/1961");
+        Person joe = new Person("Joe", "01/01/1961", null);
+        Person jim = new Person("Jim", "02/02/1962", null);
+        Person jack = new Person("Jack", "03/03/1963", new Person[]{joe, jim});
+        Person jane = new Person("Jane", "04/04/1964", null);
+        Person jill = new Person("Jill", "05/05/1965", new Person[]{joe, jim});
 
         Person[] persons = {joe, jim, jack, jane, jill};
         Person[] personCopy = Arrays.copyOf(persons, persons.length);
+
+        var jillsKids = personCopy[4].kids();
+        jillsKids[1] = jane;
 
         for (int i = 0; i < 5; i++) {
             if (persons[i] == personCopy[i]) {
