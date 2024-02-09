ec0155c7cc65c40c16fd69327223e9333e6d1cf3
Gilberto <gil_uehara@yahoo.com.br>
2024-02-09 20:37:14 -0300

    more studies

--- _16-03-ImmutableChallenges/src/dev/bank/BankAccount.java
+++ _16-03-ImmutableChallenges/src/dev/bank/BankAccount.java
@@ -1,10 +1,16 @@
 package dev.bank;
 
+import dev.dto.Transaction;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class BankAccount {
 
     public enum AccountType {CHECKING, SAVINGS}
     private final AccountType accountType;
-    private final double balance;
+    private double balance;
+    private final Map<Long, Transaction> transactions = new LinkedHashMap<>();
 
     BankAccount(AccountType accountType, double balance) {
         this.accountType = accountType;
@@ -19,8 +25,19 @@ public class BankAccount {
         return balance;
     }
 
+    public Map<Long, Transaction> getTransactions() {
+        return Map.copyOf(transactions);
+    }
+
     @Override
     public String toString() {
         return "%s $%.2f".formatted(accountType, balance);
+    }
+
+    void commitTransaction(int routingNumber, long transactionId, String customerId, double amount) {
+
+        balance += amount;
+        transactions.put(transactionId,
+                new Transaction(routingNumber, transactionId, Integer.parseInt(customerId), amount));
     }
 }
