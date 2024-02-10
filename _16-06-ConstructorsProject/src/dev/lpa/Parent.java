81d576775d29e2f468f5266071ea2ad17874d53d
Gilberto <gil_uehara@yahoo.com.br>
2024-02-10 14:50:18 -0300

    more studies

---
+++ _16-06-ConstructorsProject/src/dev/lpa/Parent.java
@@ -0,0 +1,50 @@
+package dev.lpa;
+
+public class Parent {
+
+    static {
+        System.out.println("Parent static initializer: class being constructed");
+    }
+
+    private final String name;
+    private final String dob;
+    protected final int siblings;
+
+    {
+//        name = "John Doe";
+//        dob = "01/01/1900";
+        System.out.println("In Parent Initializer");
+    }
+
+//    public Parent() {
+//        System.out.println("In Parent's No Args Constructor");
+//    }
+
+    public Parent(String name, String dob, int siblings) {
+        this.name = name;
+        this.dob = dob;
+        this.siblings = siblings;
+        System.out.println("In Parent Constructor");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    public String getDob() {
+        return dob;
+    }
+
+//    public void setDob(String dob) {
+//        this.dob = dob;
+//    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' + ", dob='" + dob + '\'';
+    }
+}
