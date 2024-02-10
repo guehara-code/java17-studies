1d2c118a336698286238aa18d3e07f91ba237ee2
Gilberto <gil_uehara@yahoo.com.br>
2024-02-10 08:38:37 -0300

    more studies

--- _16-03-ImmutableChallenges/src/dev/bank/BankCustomer.java
+++ _16-03-ImmutableChallenges/src/dev/bank/BankCustomer.java
@@ -38,6 +38,7 @@ public class BankCustomer {
                 return account;
             }
         }
+        return null;
     }
 
     @Override
--- _16-03-ImmutableChallenges/src/dev/lpa/Main.java
+++ _16-03-ImmutableChallenges/src/dev/lpa/Main.java
@@ -1,5 +1,6 @@
 package dev.lpa;
 
+import dev.bank.Bank;
 import dev.bank.BankAccount;
 import dev.bank.BankCustomer;
 
@@ -22,13 +23,20 @@ public class Main {
 //        BankCustomer joe = new BankCustomer("Joe", 500.00, 10000.00);
 //        System.out.println(joe);
 
-        BankCustomer joe = null;
+        Bank bank = new Bank(3214567);
+        bank.addCustomer("Joe", 500.00, 1000.00);
 
-        List<BankAccount> accounts = joe.getAccounts();
-        accounts.clear();
+        BankCustomer joe = bank.getCustomer("000000010000000");
         System.out.println(joe);
+
 
 //        accounts.add(new BankAccount(BankAccount.AccountType.CHECKING, 150000));
 //        System.out.println(joe);
+
+        if (bank.doTransaction(joe.getCustomerId(), BankAccount.AccountType.CHECKING, 35)) {
+            System.out.println(joe);
+        }
+
+
     }
 }
