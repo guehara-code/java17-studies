751136341ec02eca0db86085cec327c28efdfc19
Gilberto <gil_uehara@yahoo.com.br>
2024-02-11 09:14:34 -0300

    more studies

---
+++ _16-06-ConstructorsProject/src/dev/lpa/Person.java
@@ -0,0 +1,11 @@
+package dev.lpa;
+
+
+public record Person(String name, String dob) {
+
+    public Person(String name, String dob) {
+        this.name = name;
+        this.dob = dob.replace('-', '/');
+    }
+}
+
