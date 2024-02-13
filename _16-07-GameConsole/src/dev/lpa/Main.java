3ea21d82cfd409bae356c617b18ae6fc45050c77
Gilberto <gil_uehara@yahoo.com.br>
2024-02-13 09:21:53 -0300

    more studies

--- _16-07-GameConsole/src/dev/lpa/Main.java
+++ _16-07-GameConsole/src/dev/lpa/Main.java
@@ -2,6 +2,7 @@ package dev.lpa;
 
 import dev.lpa.game.GameConsole;
 import dev.lpa.game.ShooterGame;
+import dev.lpa.pirate.Pirate;
 import dev.lpa.pirate.Weapon;
 
 public class Main {
@@ -19,5 +20,8 @@ public class Main {
 
         var list = Weapon.getWeaponsByLevel(1);
         list.forEach(System.out::println);
+
+        Pirate tim = new Pirate("Tim");
+        System.out.println(tim);
     }
 }
