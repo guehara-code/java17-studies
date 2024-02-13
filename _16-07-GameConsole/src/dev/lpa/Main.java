064603c5bb4fd457d5589eb00c132451cfe61f56
Gilberto <gil_uehara@yahoo.com.br>
2024-02-13 08:56:09 -0300

    more studies

--- _16-07-GameConsole/src/dev/lpa/Main.java
+++ _16-07-GameConsole/src/dev/lpa/Main.java
@@ -2,14 +2,22 @@ package dev.lpa;
 
 import dev.lpa.game.GameConsole;
 import dev.lpa.game.ShooterGame;
+import dev.lpa.pirate.Weapon;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+//        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+//
+//        int playerIndex = console.addPlayer();
+//        console.playGame(playerIndex);
 
-        int playerIndex = console.addPlayer();
-        console.playGame(playerIndex);
+        Weapon weapon = Weapon.getWeaponByChar('P');
+        System.out.printf("weapon = " + weapon + ", hitPoints=" +
+                weapon.getHitPoints() + ", minLevel=" + weapon.getMinLevel());
+
+        var list = Weapon.getWeaponsByLevel(1);
+        list.forEach(System.out::println);
     }
 }
