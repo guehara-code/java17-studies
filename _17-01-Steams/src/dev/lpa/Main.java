d11d0e5773481b3a6686f0289496b3ac1b3c17bd
Gilberto <gil_uehara@yahoo.com.br>
2024-02-16 09:23:25 -0300

    more studies

--- _17-01-Steams/src/dev/lpa/Main.java
+++ _17-01-Steams/src/dev/lpa/Main.java
@@ -1,7 +1,6 @@
 package dev.lpa;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -13,10 +12,48 @@ public class Main {
         for (char c : "BINGO".toCharArray()) {
             for (int i = start; i < (start + 15); i++) {
                 bingoPool.add("" + c + i);
-                System.out.println("" + c + i);
+//                System.out.println("" + c + i);
             }
             start += 15;
         }
 
+        Collections.shuffle(bingoPool);
+        for (int i = 0; i < 15; i++) {
+            System.out.println(bingoPool.get(i));
+        }
+        System.out.println("-------------------------------------------");
+
+//        List<String> firstOnes = bingoPool.subList(0, 15);
+        List<String> firstOnes = new ArrayList<>(bingoPool.subList(0, 15));
+        firstOnes.sort(Comparator.naturalOrder());
+        firstOnes.replaceAll(s -> {
+            if ( s.indexOf('G') == 0 || s.indexOf("O") == 0) {
+                String updated = s.charAt(0) + "-" + s.substring(1);
+                System.out.print(updated + " ");
+                return updated;
+            }
+            return s;
+        });
+        System.out.println("\n-----------------------------------------");
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println(bingoPool.get(i));
+        }
+        System.out.println("-------------------------------------------");
+
+        bingoPool.stream()
+                .limit(15)
+                .filter(s -> s.indexOf('G') == 0 || s.indexOf("O") == 0)
+                .map(s -> s.charAt(0) + "-" + s.substring(1))
+                .sorted()
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println("\n-----------------------------------------");
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println(bingoPool.get(i));
+        }
     }
+
+
 }
