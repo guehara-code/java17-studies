c2f6e1fdd75b1a69f6b63835d239e4cc73852068
Gilberto <gil_uehara@yahoo.com.br>
2024-02-20 09:25:13 -0300

    more studies

---
+++ _17-04-StreamTerminal/src/dev/lpa/Seat.java
@@ -0,0 +1,10 @@
+package dev.lpa;
+
+import java.util.Random;
+
+public record Seat(char rowMarker, int seatNumber, boolean isReserved) {
+
+    public Seat(char rowMarker, int seatNumber) {
+        this(rowMarker, seatNumber, false);
+    }
+}
