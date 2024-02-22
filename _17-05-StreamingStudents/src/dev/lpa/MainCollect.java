4182f9399a932c49f893ce08dccc891eb55138c3
Gilberto <gil_uehara@yahoo.com.br>
2024-02-22 09:22:19 -0300

    more studies

--- _17-05-StreamingStudents/src/dev/lpa/MainCollect.java
+++ _17-05-StreamingStudents/src/dev/lpa/MainCollect.java
@@ -1,7 +1,11 @@
 package dev.lpa;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,5 +26,24 @@ public class MainCollect {
                 .collect(Collectors.toSet());
         System.out.println("# of Australian Students = " + australianStudents.size());
 
+        Set<Student> underThirty = students.stream()
+                .filter((s) -> s.getAgeEnrolled() < 30)
+                .collect(Collectors.toSet());
+        System.out.println("# of Under Thirty Students = " + underThirty.size());
+
+        Set<Student> youngAussies1 = new TreeSet<>(Comparator.comparing(Student::getStudentId));
+        youngAussies1.addAll(australianStudents);
+        youngAussies1.retainAll(underThirty);
+        youngAussies1.forEach((s) -> System.out.print(s.getStudentId() + " "));
+        System.out.println();
+
+        Set<Student> youngAussies2 = students.stream()
+                .filter((s) -> s.getAgeEnrolled() < 30)
+                .filter((s) -> s.getCountryCode().equals("AU"))
+                .collect(() -> new TreeSet<>(Comparator.comparing(
+                        Student::getStudentId)), TreeSet::add, TreeSet::addAll);
+
+        youngAussies2.forEach((s) -> System.out.print(s.getStudentId() + " "));
+        System.out.println();
     }
 }
