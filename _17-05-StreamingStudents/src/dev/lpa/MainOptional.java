d422fc02cdc8cbec9de52c6cf26f6182de672aa9
Gilberto <gil_uehara@yahoo.com.br>
2024-02-23 14:14:14 -0300

    more studies

--- _17-05-StreamingStudents/src/dev/lpa/MainOptional.java
+++ _17-05-StreamingStudents/src/dev/lpa/MainOptional.java
@@ -1,5 +1,6 @@
 package dev.lpa;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -17,17 +18,34 @@ public class MainOptional {
                 Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
                         .limit(1000)
                         .collect(Collectors.toList());
+
+        Optional<Student> o1 = getStudent(new ArrayList<>(), "first");
+        System.out.println("Empty = " + o1.isEmpty() + ", Present = " + o1.isPresent());
+        System.out.println(o1);
+//        System.out.println(o1.get());
+        o1.ifPresentOrElse(System.out::println, () -> System.out.println("---> Empty"));
+
+
+//        students.add(0, null);
+        Optional<Student> o2 = getStudent(students, "first");
+        System.out.println("Empty = " + o2.isEmpty() + ", Present = " + o2.isPresent());
+        System.out.println(o2);
+        o2.ifPresent(System.out::println);
+
+        Student firstStudent = o2.orElse(null);
+        long id = (firstStudent == null) ? -1 : firstStudent.getStudentId();
+        System.out.println("firstStudent's id is " + id);
     }
 
     private static Optional<Student> getStudent(List<Student> list, String type) {
 
         if (list == null || list.size() == 0) {
-            return null;
+            return Optional.empty();
         } else if (type.equals("first")) {
-            return Optional.of(list.get(0));
+            return Optional.ofNullable(list.get(0));
         } else if (type.equals("last")) {
-            return Optional.of(list.get(list.size() - 1));
+            return Optional.ofNullable(list.get(list.size() - 1));
         }
-        return Optional.of(list.get(new Random().nextInt(list.size())));
+        return Optional.ofNullable(list.get(new Random().nextInt(list.size())));
     }
 }
