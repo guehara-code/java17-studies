3196734429fbbc258dc00a9647060949aa8fb052
Gilberto <gil_uehara@yahoo.com.br>
2024-02-25 09:32:12 -0300

    more studies

--- _17-05-StreamingStudents/src/dev/lpa/MainFinalChallenge.java
+++ _17-05-StreamingStudents/src/dev/lpa/MainFinalChallenge.java
@@ -2,6 +2,7 @@ package dev.lpa;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainFinalChallenge {
@@ -23,6 +24,31 @@ public class MainFinalChallenge {
                 .stream()
                 .mapToInt(Student::getYearEnrolled)
                 .summaryStatistics());
+
+        students.subList(0, 10).forEach(System.out::println);
+
+        System.out.println(students
+                .stream()
+                .mapToInt(s -> s.getEngagementMap().size())
+                .summaryStatistics());
+
+        var mappedActivity = students.stream()
+                .flatMap(s -> s.getEngagementMap().values().stream())
+                .collect(Collectors.groupingBy(CourseEngagement::getCourse, Collectors.counting()));
+
+        mappedActivity.forEach((k, v) -> System.out.println(k + " " + v));
+
+        var classCounts = students.stream()
+                .collect(Collectors.groupingBy(s -> s.getEngagementMap().size(), Collectors.counting()));
+
+        classCounts.forEach((k, v) -> System.out.println(k + " " + v));
+
+        var percentages = students.stream()
+                .flatMap(s -> s.getEngagementMap().values().stream())
+                .collect(Collectors.groupingBy(CourseEngagement::getCourse,
+                        Collectors.summarizingDouble(CourseEngagement::getPercentComplete)));
+
+        percentages.forEach((k, v) -> System.out.println(k + " " + v));
     }
 
 }
