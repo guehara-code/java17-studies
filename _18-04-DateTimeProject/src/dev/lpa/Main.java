aa52d4f246b43b72505d43c25a9cf0cb09cc6cd6
Gilberto <gil_uehara@yahoo.com.br>
2024-02-28 16:54:05 -0300

    more studies

--- _18-04-DateTimeProject/src/dev/lpa/Main.java
+++ _18-04-DateTimeProject/src/dev/lpa/Main.java
@@ -3,6 +3,7 @@ package dev.lpa;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
 
@@ -36,5 +37,30 @@ public class Main {
         System.out.println(May5.get(ChronoField.MONTH_OF_YEAR));
         System.out.println(May5.get(ChronoField.DAY_OF_MONTH));
         System.out.println(May5.get(ChronoField.DAY_OF_YEAR));
+
+        System.out.println(May5.withYear(2000));
+        System.out.println(May5.withMonth(3));
+        System.out.println(May5.withDayOfMonth(4));
+        System.out.println(May5.withDayOfYear(126));
+        System.out.println(May5);
+        System.out.println(May5.with(ChronoField.DAY_OF_YEAR, 126));
+        System.out.println(May5.plusYears(1));
+        System.out.println(May5.plusMonths(12));
+        System.out.println(May5.plusDays(365));
+        System.out.println(May5.plusWeeks(52));
+        System.out.println(May5.plus(365, ChronoUnit.DAYS));
+
+        System.out.println("May5 > today? " + May5.isAfter(today));
+        System.out.println("today > May5? " + May5.isBefore(today));
+        System.out.println("May5 > today? " + May5.compareTo(today));
+        System.out.println("today > May5? " + today.compareTo(May5));
+
+        System.out.println("today = now ? " + today.compareTo(LocalDate.now()));
+        System.out.println("today = now ? " + today.equals(LocalDate.now()));
+
+        System.out.println(today.isLeapYear());
+        System.out.println(May5.minusYears(3).isLeapYear());
+
+
     }
 }
