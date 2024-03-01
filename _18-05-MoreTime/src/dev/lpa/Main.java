e3b8ce3cfd18206bd2a72838017cd38f53595de7
Gilberto <gil_uehara@yahoo.com.br>
2024-03-01 17:08:33 -0300

    more studies

--- _18-05-MoreTime/src/dev/lpa/Main.java
+++ _18-05-MoreTime/src/dev/lpa/Main.java
@@ -3,6 +3,8 @@ package dev.lpa;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
 import java.util.HashSet;
@@ -70,5 +72,46 @@ public class Main {
 
         Period timePast = Period.between(LocalDate.EPOCH, dob.toLocalDate());
         System.out.println(timePast);
+
+        Duration timeSince =
+                Duration.between(Instant.EPOCH, dob.toInstant(ZoneOffset.UTC));
+        System.out.println(timeSince);
+
+        LocalDateTime dob2 = dob.plusYears(2).plusMonths(4)
+                .plusDays(4).plusHours(7).plusMinutes(14)
+                .plusSeconds(37);
+
+        System.out.println("Your 2nh kid's birthdate, Here Time = " + dob2.format(
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+
+        Period timePast2 = Period.between(LocalDate.EPOCH, dob2.toLocalDate());
+        System.out.println(timePast2);
+
+        Duration timeSince2 =
+                Duration.between(Instant.EPOCH, dob2.toInstant(ZoneOffset.UTC));
+        System.out.println(timeSince2);
+
+        for (ChronoUnit u : ChronoUnit.values()) {
+            if (u.isSupportedBy(LocalDate.EPOCH)) {
+                long val = u.between(LocalDate.EPOCH,
+                        dob2.toLocalDate());
+                System.out.println(u + " past = " + val);
+            } else {
+                System.out.println("-- Not supported: " + u);
+            }
+        }
+
+        LocalDateTime ldt = LocalDateTime.ofInstant(Instant.EPOCH,
+                ZoneOffset.UTC);
+        for (ChronoUnit u : ChronoUnit.values()) {
+            if (u.isSupportedBy(ldt)) {
+                long val = u.between(ldt,
+                        dob2);
+                System.out.println(u + " past = " + val);
+            } else {
+                System.out.println("-- Not supported: " + u);
+            }
+        }
+
     }
 }
