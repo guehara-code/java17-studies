3d4fa6e61f310c648c03eb8d2d1f7cdc8a894b9a
Gilberto <gil_uehara@yahoo.com.br>
2024-03-03 09:02:00 -0300

    more studies

--- _18-06-LocaleProject/src/dev/lpa/Main.java
+++ _18-06-LocaleProject/src/dev/lpa/Main.java
@@ -1,12 +1,15 @@
 package dev.lpa;
 
+import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Currency;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
@@ -44,8 +47,23 @@ public class Main {
             System.out.print(String.format(locale, "\t%1$tA, %1$tB, %1$te, %1$tY %n", may5));
 
             NumberFormat decimalInfo = NumberFormat.getNumberInstance(locale);
+            decimalInfo.setMaximumFractionDigits(6);
             System.out.println(decimalInfo.format(123456789.123456));
+
+            NumberFormat currency = NumberFormat.getCurrencyInstance(locale);
+            Currency localCurrency = Currency.getInstance(locale);
+            System.out.println(currency.format(555.555) + " [" +
+                    localCurrency.getCurrencyCode() + "] " +
+                    localCurrency.getDisplayName(locale) + "/" +
+                    localCurrency.getDisplayName());
         }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the loan amount: ");
+        scanner.useLocale(Locale.ITALY);
+        BigDecimal myLoan = scanner.nextBigDecimal();
+        NumberFormat decimalInfo = NumberFormat.getNumberInstance(Locale.ITALY);
+        System.out.println("My Loan " + decimalInfo.format(myLoan));
 
 
 
