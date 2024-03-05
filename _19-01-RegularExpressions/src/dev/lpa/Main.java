28e62ed59c776e7576a53185bad944b6bc1c07a3
Gilberto <gil_uehara@yahoo.com.br>
2024-03-05 14:49:50 -0300

    more studies

--- _19-01-RegularExpressions/src/dev/lpa/Main.java
+++ _19-01-RegularExpressions/src/dev/lpa/Main.java
@@ -1,5 +1,8 @@
 package dev.lpa;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -26,6 +29,46 @@ public class Main {
             System.out.println("Pattern: " + pattern + " => " + output);
         }
 
+        // Song of the Witches in MacBeth, a Play by Shakespeare
+        String paragraph = """
+                Double, double toil and trouble;
+                Fire burn and caldron bubble.
+                Fillet of a genny snake,
+                In the caldron boil and bake
+                Eye of newt and toe of frog,
+                Wool of bat and tongue of dog,
+                Adder's fork and blind-worm's sting,
+                Lizard's leg and howlet's wing,
+                For a charm of powerful trouble,
+                Like a hell-broth boil and bubble.
+                """;
+
+        String[] lines = paragraph.split("\\R");
+        System.out.println("This paragraph has " + lines.length + " lines");
+        String[] words = paragraph.split("\\s");
+        System.out.println("This paragraph has " + words.length + " words");
+        System.out.println(paragraph.replaceAll("[a-zA-Z]+ble", "[GRUB]"));
+
+        Scanner scanner = new Scanner(paragraph);
+        System.out.println(scanner.delimiter());
+        scanner.useDelimiter("\\R");
+
+//        while (scanner.hasNext()) {
+//            String element = scanner.next();
+//            System.out.println(element);
+//        }
+
+//        scanner.tokens()
+//                .map(s -> s.replaceAll("\\p{Punct}", ""))
+//                .flatMap(s -> Arrays.stream(s.split("\\s")))
+//                .filter(s -> s.matches("[a-zA-Z]+ble"))
+//                .forEach(System.out::println);
+
+        System.out.println(scanner.findInLine("[a-zA-Z]+ble"));
+        System.out.println(scanner.findInLine("[a-zA-Z]+ble"));
+        scanner.close();
+
+        scanner.close();
 
 
     }
