a057b161f221694febee08ea5dfdefe45ac7e270
Gilberto <gil_uehara@yahoo.com.br>
2024-03-07 09:13:53 -0300

    more studies

---
+++ _19-03-PatternMatching/src/dev/lpa/Review.java
@@ -0,0 +1,30 @@
+package dev.lpa;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Review {
+
+    public static void main(String[] args) {
+
+
+        String phoneList = """
+                (800) 123-4567
+                (800)123-4567
+                (800) 123 4567
+                800-123-4567
+                800 123-4567
+                800 123 4567
+                8001234567
+                """;
+
+        Pattern phonePattern =
+                Pattern.compile("\\([0-9]{3}\\) [0-9]{3}-[0-9]{4}");
+
+        Matcher phoneMatcher = phonePattern.matcher(phoneList);
+        phoneMatcher.results().forEach(mr -> System.out.println(mr.group()));
+
+    }
+}
