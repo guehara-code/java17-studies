68451ed095bcb0c525b51d70c6626c2d4ae1d962
Gilberto <gil_uehara@yahoo.com.br>
2024-03-07 09:29:15 -0300

    more studies

--- _19-03-PatternMatching/src/dev/lpa/Review.java
+++ _19-03-PatternMatching/src/dev/lpa/Review.java
@@ -21,10 +21,23 @@ public class Review {
                 """;
 
         Pattern phonePattern =
-                Pattern.compile("\\([0-9]{3}\\) [0-9]{3}-[0-9]{4}");
+                Pattern.compile("\\(*[0-9]{3}[)\\s-]*\\d{3}[\\s-]*\\p{Digit}{4}");
 
         Matcher phoneMatcher = phonePattern.matcher(phoneList);
         phoneMatcher.results().forEach(mr -> System.out.println(mr.group()));
 
+        String htmlSnippets = """
+                
+                <H1>My Heading</H1>
+                <h2>Sub-heading</h2>
+                <p>This is a paragraph about something.</p>
+                <p style="abc">This is another paragraph about something else.</p>
+                <h3 id="third">Summary</h3>
+                <br/>
+                <p>Testing</p>
+                """;
+
+        Pattern htmlPattern =
+                Pattern.compile("<(\\w+)[^>]([^\\v</>]*)(</\\1>)*");
     }
 }
