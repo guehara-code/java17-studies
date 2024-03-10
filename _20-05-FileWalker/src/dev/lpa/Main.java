9b6a67a6192810610c8af0c4857b917751dc14a3
Gilberto <gil_uehara@yahoo.com.br>
2024-03-10 10:45:44 -0300

    more studies

--- _20-05-FileWalker/src/dev/lpa/Main.java
+++ _20-05-FileWalker/src/dev/lpa/Main.java
@@ -22,13 +22,38 @@ public class Main {
 
     private static class StatsVisitor extends SimpleFileVisitor<Path> {
 
+        private int level;
+
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
 
             Objects.requireNonNull(file);
             Objects.requireNonNull(attrs);
-            System.out.println(file.getFileName());
+            System.out.println("\t".repeat(level) + file.getFileName());
             return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            Objects.requireNonNull(dir);
+            Objects.requireNonNull(attrs);
+
+            level++;
+            System.out.println("\t".repeat(level + 1) + dir.getFileName());
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+
+            Objects.requireNonNull(dir);
+
+//            if (exc != null) {
+//                throw exc;
+
+            level--;
+            return FileVisitResult.CONTINUE;
+
         }
     }
 }
