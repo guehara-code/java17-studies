e2b6025d3433e0a99a48a25c9f264346f788d8b2
Gilberto <gil_uehara@yahoo.com.br>
2024-03-11 17:03:32 -0300

    mores studies

--- _20-07-ScannerProject/src/dev/lpa/Main.java
+++ _20-07-ScannerProject/src/dev/lpa/Main.java
@@ -1,28 +1,44 @@
 package dev.lpa;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.MatchResult;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        try (Scanner scanner = new Scanner(new File("file.txt"))) {
+//        try (Scanner scanner = new Scanner(new File("text.txt"))) {
+//        try (Scanner scanner = new Scanner(Path.of("fixedWidth.txt"))) {
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("fixedWidth.txt")))) {
 //            while (scanner.hasNextLine()) {
 //                System.out.println(scanner.nextLine());
 //            }
 //            System.out.println(scanner.delimiter());
 //            scanner.useDelimiter("$");
 //            scanner.tokens().forEach(System.out::println);
-            scanner.findAll("[A-Za-z]{10,}")
-                    .map(MatchResult::group)
+//            scanner.findAll("[A-Za-z]{10,}")
+//                    .map(MatchResult::group)
+//                    .distinct()
+//                    .sorted()
+//                    .forEach(System.out::println);
+            var results = scanner.findAll("(.{15})(.{3})(.{12})(.{8})(.{2}).*")
+                    .skip(1)
+//                    .map(m -> m.group(2))
+                    .map(m -> m.group(3).trim())
                     .distinct()
                     .sorted()
-                    .forEach(System.out::println);
+                    .toArray(String[]::new);
+            System.out.println(Arrays.toString(results));
 
-        } catch (FileNotFoundException e) {
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
