8c596181b5eb83dfddd447fd5be9fe4465a90fbd
Gilberto <gil_uehara@yahoo.com.br>
2024-03-13 09:04:07 -0300

    more studies

--- _20-10-WritingFiles/src/dev/lpa/Main.java
+++ _20-10-WritingFiles/src/dev/lpa/Main.java
@@ -4,6 +4,11 @@ import dev.lpa.student.Course;
 import dev.lpa.student.Student;
 import dev.lpa.student.StudentDemographics;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -24,7 +29,28 @@ public class Main {
                 .limit(5)
                 .toList();
 
-        System.out.println(header);
-        students.forEach(s -> s.getEngagementRecords().forEach(System.out::println));
+//        System.out.println(header);
+//        students.forEach(s -> s.getEngagementRecords().forEach(System.out::println));
+
+        Path path = Path.of("students.csv");
+//        try {
+//            Files.writeString(path, header);
+//            for (Student student : students) {
+//                Files.write(path, student.getEngagementRecords(), StandardOpenOption.APPEND);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        try {
+            List<String> data = new ArrayList<>();
+            data.add(header);
+            for (Student student : students) {
+                data.addAll(student.getEngagementRecords());
+            }
+            Files.write(path, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
