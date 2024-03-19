bb629d855d5e528f13daed93765e94035e566683
Gilberto <gil_uehara@yahoo.com.br>
2024-03-18 21:04:16 -0300

    more studies

--- _21-01-Threads/src/dev/lpa/Main.java
+++ _21-01-Threads/src/dev/lpa/Main.java
@@ -21,6 +21,21 @@ public class Main {
         CustomThread customThread = new CustomThread();
         customThread.start();
 
+        Runnable myRunnable = () -> {
+
+            for (int i = 1; i <= 8; i++) {
+                System.out.print(" 2 ");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(250);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread myThread = new Thread(myRunnable);
+        myThread.start();
+
         for (int  i = 1; i <= 3; i++) {
             System.out.print(" 0 ");
             try {
