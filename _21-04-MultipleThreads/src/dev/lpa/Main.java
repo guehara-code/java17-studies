0d11e36c5b7d244d0f227e3337ab66308f83d67a
Gilberto <gil_uehara@yahoo.com.br>
2024-03-20 15:52:56 -0300

    more studies:

--- _21-04-MultipleThreads/src/dev/lpa/Main.java
+++ _21-04-MultipleThreads/src/dev/lpa/Main.java
@@ -8,13 +8,18 @@ public class Main {
 
         StopWatch stopWatch = new StopWatch(TimeUnit.SECONDS);
         Thread green = new Thread(stopWatch::countDown, ThreadColor.ANSI_GREEN.name());
+        Thread purple = new Thread(() -> stopWatch.countDown(7), ThreadColor.ANSI_PURPLE.name());
+        Thread red = new Thread(stopWatch::countDown, ThreadColor.ANSI_RED.name());
         green.start();
+        purple.start();
+        red.start();
 
     }
 }
 
 class StopWatch {
     private TimeUnit timeUnit;
+    private int i;
 
     public StopWatch(TimeUnit timeUnit) {
         this.timeUnit = timeUnit;
@@ -35,7 +40,7 @@ class StopWatch {
             // User may pass a bad color name, Will just ignore this error.
         }
         String color = threadColor.color();
-        for (int i = unitCount; i > 0; i--) {
+        for ( i = unitCount; i > 0; i--) {
             try {
                 timeUnit.sleep(1);
             } catch (InterruptedException e) {
