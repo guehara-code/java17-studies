782ce941100ae8c7608b58b8151d3104b1949244
Gilberto <gil_uehara@yahoo.com.br>
2024-03-21 17:18:35 -0300

    more studies

--- _21-05-Synchronization/src/dev/lpa/BankAccount.java
+++ _21-05-Synchronization/src/dev/lpa/BankAccount.java
@@ -3,9 +3,24 @@ package dev.lpa;
 public class BankAccount {
 
     private double balance;
+    private String name;
+    private final Object lockName = new Object();
+    private final Object lockBalance = new Object();
 
-    public BankAccount(double balance) {
+    public BankAccount(String name, double balance) {
         this.balance = balance;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        synchronized (lockName) {
+            this.name = name;
+            System.out.println("Updated name = " + this.name);
+        }
     }
 
     public double getBalance() {
@@ -21,11 +36,21 @@ public class BankAccount {
             throw new RuntimeException(e);
         }
 
-        synchronized (this) {
+        synchronized (lockBalance) {
             double origBalance = balance;
             balance += amount;
             System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f) : NEW BALANCE = %.0f%n",
                     origBalance, amount, balance);
+            addPromoDollars(amount);
+        }
+    }
+    private void addPromoDollars(double amount) {
+
+        if (amount >= 5000) {
+            synchronized (lockBalance) {
+                System.out.println("Congratulations, you earned a promotional deposit.");
+                balance += 25;
+            }
         }
     }
 
@@ -47,4 +72,6 @@ public class BankAccount {
                     origBalance, amount);
         }
     }
+
+
 }
