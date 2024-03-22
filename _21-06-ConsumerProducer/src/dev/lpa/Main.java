81da85a5476e4dd46244bca52880743d2ae2c624
Gilberto <gil_uehara@yahoo.com.br>
2024-03-22 16:57:59 -0300

    more studies

--- _21-06-ConsumerProducer/src/dev/lpa/Main.java
+++ _21-06-ConsumerProducer/src/dev/lpa/Main.java
@@ -1,6 +1,9 @@
 package dev.lpa;
 
 
+import java.io.IOException;
+import java.util.Random;
+
 class MessageRepository {
 
     private String message;
@@ -9,18 +12,28 @@ class MessageRepository {
     public synchronized String read() {
 
         while (!hasMessage) {
-
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         hasMessage = false;
+        notifyAll();
         return message;
     }
 
     public synchronized void write(String message) {
 
         while (hasMessage) {
-
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         hasMessage = true;
+        notifyAll();
         this.message = message;
     }
 
@@ -29,13 +42,58 @@ class MessageRepository {
 class MessageWriter implements Runnable {
 
     private MessageRepository outgoingMessage;
-    private final String test = """
-            
-      
-            """
+    private final String text = """
+            Humpty Dumpty sat on a wall,
+            Humpty Dumpty had a great fall,
+            All the king's horses and all king's men,
+            Couldn't put Humpty together again.""";
+
+    public MessageWriter(MessageRepository outgoingMessage) {
+        this.outgoingMessage = outgoingMessage;
+    }
+
     @Override
     public void run() {
 
+        Random random = new Random();
+        String[] lines = text.split("\n");
+
+        for (int i = 0; i < lines.length; i++) {
+            outgoingMessage.write(lines[i]);
+            try {
+                Thread.sleep(random.nextInt(500, 2000));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        outgoingMessage.write("Finished");
+    }
+
+}
+
+class MessageReader implements Runnable {
+
+    private MessageRepository incomingMessage;
+
+    public MessageReader(MessageRepository incomingMessage) {
+        this.incomingMessage = incomingMessage;
+    }
+
+    @Override
+    public void run() {
+
+        Random random = new Random();
+        String latestMessage = "";
+
+        do {
+            try {
+                Thread.sleep(random.nextInt(500, 2000));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            latestMessage = incomingMessage.read();
+            System.out.println(latestMessage);
+        } while (!latestMessage.equals("Finished"));
     }
 }
 
@@ -43,6 +101,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        MessageRepository messageRepository = new MessageRepository();
+
+        Thread reader = new Thread(new MessageReader(messageRepository));
+        Thread writer = new Thread(new MessageWriter(messageRepository));
+
+        reader.start();
+        writer.start();
     }
 
 }
