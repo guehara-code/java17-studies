7219b9aaf5375ea2bd9d3ac1f10ec49807852143
guehara-code <gil_uehara@yahoo.com.br>
2024-03-24 20:01:23 -0300

    more studies

--- _21-08-Executors/src/dev/lpa/Main.java
+++ _21-08-Executors/src/dev/lpa/Main.java
@@ -1,29 +1,98 @@
 package dev.lpa;
 
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+class ColorThreadFactory implements ThreadFactory {
+
+    private String threadName;
+
+    public ColorThreadFactory(ThreadColor color) {
+        this.threadName = color.name();
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+
+        Thread thread = new Thread(r);
+        thread.setName(threadName);
+        return  thread;
+    }
+}
+
+
 
 public class Main {
 
     public static void main(String[] args) {
+
+        var blueExecutor = Executors.newSingleThreadExecutor(
+                new ColorThreadFactory(ThreadColor.ANSI_BLUE)
+        );
+        blueExecutor.execute(Main::countDown);
+        blueExecutor.shutdown();
+
+        boolean isDone = false;
+        try {
+            isDone = blueExecutor.awaitTermination(500, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (isDone) {
+            System.out.println("Blue finished, starting Yellow");
+            var yellowExecutor = Executors.newSingleThreadExecutor(
+                    new ColorThreadFactory(ThreadColor.ANSI_YELLOW)
+            );
+            yellowExecutor.execute(Main::countDown);
+            yellowExecutor.shutdown();
+            try {
+                isDone = yellowExecutor.awaitTermination(500, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            if (isDone) {
+                System.out.println("Yellow finished, starting Red");
+                var redExecutor = Executors.newSingleThreadExecutor(
+                        new ColorThreadFactory(ThreadColor.ANSI_RED)
+                );
+                redExecutor.execute(Main::countDown);
+                redExecutor.shutdown();
+                try {
+                    isDone = redExecutor.awaitTermination(500, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                if (isDone) {
+                    System.out.println("All processes completed");
+                }
+            }
+        }
+    }
+
+    public static void notmain(String[] args) {
 
         Thread blue = new Thread(Main::countDown, ThreadColor.ANSI_BLUE.name());
         Thread yellow = new Thread(Main::countDown, ThreadColor.ANSI_YELLOW.name());
         Thread red = new Thread(Main::countDown, ThreadColor.ANSI_RED.name());
 
         blue.start();
-        yellow.start();
-        red.start();
-
         try {
             blue.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
+        yellow.start();
         try {
             yellow.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        red.start();
         try {
             red.join();
         } catch (InterruptedException e) {
