9ed89402760de73f46c9a835bbbd3ab7e2cd0086
guehara-code <gil_uehara@yahoo.com.br>
2024-03-25 20:58:28 -0300

    more studies

---
+++ _21-10-SchedulingTasks/src/dev/lpa/Main.java
@@ -0,0 +1,53 @@
+package dev.lpa;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var dtf = DateTimeFormatter.ofLocalizedDateTime(
+                FormatStyle.MEDIUM,
+                FormatStyle.LONG
+        );
+
+        Callable<ZonedDateTime> waitThenDoIt = () -> {
+            ZonedDateTime zdt = null;
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                zdt = ZonedDateTime.now();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return zdt;
+        };
+
+        var threadPool = Executors.newFixedThreadPool(2);
+        List<Callable<ZonedDateTime>> list = Collections.nCopies(4, waitThenDoIt);
+
+        try {
+            System.out.println("---> " + ZonedDateTime.now().format(dtf));
+            List<Future<ZonedDateTime>> futureList = threadPool.invokeAll(list);
+            for (Future<ZonedDateTime> result : futureList) {
+                try {
+                    System.out.println(result.get(1, TimeUnit.SECONDS).format(dtf));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            threadPool.shutdown();
+        }
+
+    }
+}
