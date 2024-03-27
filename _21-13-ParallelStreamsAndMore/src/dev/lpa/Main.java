d98c2325c87a7dd00396e2539a38440227d8ea29
guehara-code <gil_uehara@yahoo.com.br>
2024-03-27 14:58:48 -0300

    more studies

---
+++ _21-13-ParallelStreamsAndMore/src/dev/lpa/Main.java
@@ -0,0 +1,40 @@
+package dev.lpa;
+
+import java.util.Comparator;
+import java.util.Random;
+import java.util.stream.Stream;
+
+record Person(String firstName, String lastName, int age) {
+
+    private final static String[] firsts = {
+            "Able", "Bob", "Charlie", "Donna", "Eve", "Fred"
+    };
+    private final static String[] lasts = {
+            "Norton", "OHara", "Petersen", "Quincy", "Richardson", "Smith"
+    };
+
+    private final static Random random = new Random();
+
+    public Person() {
+        this(firsts[random.nextInt(firsts.length)],
+                lasts[random.nextInt(lasts.length)],
+                random.nextInt(18, 100));
+    }
+
+    @Override
+    public String toString() {
+        return "%s, %s (%d)".formatted(lastName, firstName, age);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stream.generate(Person::new)
+                .limit(10)
+                .sorted(Comparator.comparing(Person::lastName))
+                .forEach(System.out::println);
+
+    }
+}
