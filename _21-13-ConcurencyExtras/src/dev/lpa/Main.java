1137cf205b6433a0e097c0a99d53b10c2b1acd78
guehara-code <gil_uehara@yahoo.com.br>
2024-03-29 20:46:49 -0300

    more studies

---
+++ _21-13-ConcurencyExtras/src/dev/lpa/Main.java
@@ -0,0 +1,62 @@
+package dev.lpa;
+
+import java.util.Collections;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.Executors;
+
+record Student(String name, int enrolledYear, int studentId) implements Comparable<Student> {
+
+
+    @Override
+    public int compareTo(Student o) {
+        return this.studentId - o.studentId;
+    }
+}
+
+class StudentId {
+
+    private int id = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNextId() {
+        return id++;
+    }
+}
+
+public class Main {
+
+    private static Random random = new Random();
+
+    private static ConcurrentSkipListSet<Student> studentSet =
+            new ConcurrentSkipListSet<>();
+
+    public static void main(String[] args) {
+
+        StudentId idGenerator = new StudentId();
+        Callable<Student> studentMaker = () -> {
+            int studentId = idGenerator.getNextId();
+            Student student = new Student("Tim " + studentId,
+                    random.nextInt(2018, 2025), studentId);
+            studentSet.add(student);
+            return student;
+        };
+
+        var executor = Executors.newCachedThreadPool();
+
+        try {
+            var futures = executor.invokeAll(
+                    Collections.nCopies(10, studentMaker));
+            studentSet.forEach(System.out::println);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        executor.shutdown();
+
+    }
+}
