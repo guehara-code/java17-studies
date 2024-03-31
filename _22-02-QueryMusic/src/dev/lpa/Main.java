35ada9e5c4f38390f30116ec02cb2accc47ede9a
guehara-code <gil_uehara@yahoo.com.br>
2024-03-31 20:22:57 -0300

    more studies

--- _22-02-QueryMusic/src/dev/lpa/Main.java
+++ _22-02-QueryMusic/src/dev/lpa/Main.java
@@ -1,5 +1,11 @@
 package dev.lpa;
 
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Main {
@@ -7,8 +13,27 @@ public class Main {
     public static void main(String[] args) {
 
         Properties props = new Properties();
+        try {
+            props.load(Files.newInputStream(Path.of("music_properties")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        // git test
+        var dataSource = new MariaDbDataSource();
+        try {
+            dataSource.setUrl(props.getProperty("url"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (var connection = dataSource.getConnection(
+                props.getProperty("user"),
+                System.getenv("MYSQL_PASS"))) {
+            System.out.println("Success!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
