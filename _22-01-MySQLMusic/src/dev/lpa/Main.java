34cc4991b69ef645078cc865e0b37272dd86638a
guehara-code <gil_uehara@yahoo.com.br>
2024-03-31 10:33:48 -0300

    more studies

---
+++ _22-01-MySQLMusic/src/dev/lpa/Main.java
@@ -0,0 +1,36 @@
+package dev.lpa;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+
+public class Main {
+
+    private final static String CONN_STRING = "jdbc:mariadb://localhost:3306/music";
+
+//    ("jdbc:mariadb://localhost:3306/DB?user=root&password=myPassword");
+
+    public static void main(String[] args) {
+
+        String username = JOptionPane.showInputDialog(
+                null, "Enter DB Username");
+
+        JPasswordField pf = new JPasswordField();
+        int okCxl = JOptionPane.showConfirmDialog(null, pf,
+                "Enter DB Password", JOptionPane.OK_CANCEL_OPTION);
+        final char[] password =
+                (okCxl == JOptionPane.OK_OPTION) ? pf.getPassword() : null;
+
+
+        try (Connection connection = DriverManager.getConnection(
+                CONN_STRING, username, String.valueOf(password))) {
+            System.out.println("Success!! Connection made to the music database");
+            Arrays.fill(password, ' ');
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
