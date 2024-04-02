abbbd34a2462c52bf5bf37f9670ad9c4be081c86
guehara-code <gil_uehara@yahoo.com.br>
2024-04-02 09:08:58 -0300

    more studies

--- _22-02-QueryMusic/src/dev/lpa/MusicDML.java
+++ _22-02-QueryMusic/src/dev/lpa/MusicDML.java
@@ -1,9 +1,6 @@
 package dev.lpa;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MusicDML {
 
@@ -21,12 +18,55 @@ public class MusicDML {
                 System.getenv("MYSQL_PASS"));
              Statement statement = connection.createStatement();
         ) {
-            String artist = "Elf";
-            String query = "SELECT * FROM artists WHERE artist_name='%s'".formatted(artist);
-            boolean result = statement.execute(query);
-            System.out.println("result = " + result);
+//            String artist = "Neil Young";
+//            String query = "SELECT * FROM artists WHERE artist_name='%s'".formatted(artist);
+//            boolean result = statement.execute(query);
+//            System.out.println("result = " + result);
+//            var rs = statement.getResultSet();
+//            boolean found = (rs != null && rs.next());
+//            System.out.println("Artist was " + (found ? "found" : "not found"));
+
+            String tableName = "music.artists";
+            String columnName = "artist_name";
+            String columnValue = "Elf";
+            if (!executeSelect(statement,tableName, columnName, columnValue)) {
+                System.out.println("Maybe we should add this record");
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static boolean printRecords(ResultSet resultSet) throws SQLException {
+
+        boolean foundData = false;
+        var meta = resultSet.getMetaData();
+
+        System.out.println("=============================");
+
+        for (int i = 1; i <= meta.getColumnCount(); i++) {
+            System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
+        }
+        System.out.println();
+
+        while (resultSet.next()) {
+            for (int i = 1; i <= meta.getColumnCount(); i++) {
+                System.out.printf("%-15s", resultSet.getString(i));
+            }
+            System.out.println();
+            foundData = true;
+        }
+
+        return foundData;
+    }
+
+    private static boolean executeSelect(Statement statement, String table,
+                                         String columnName, String columnValue) throws SQLException {
+        String query = "SELECT * FROM %s WHERE %s='%s'".formatted(table, columnName, columnValue);
+        var rs = statement.executeQuery(query);
+        if (rs != null) {
+            return printRecords(rs);
+        }
+        return false;
     }
 }
