1e76fc22d71d11dd96bf1fc9494b58bcf647aa43
guehara-code <gil_uehara@yahoo.com.br>
2024-04-02 16:51:42 -0300

    more studies

--- _22-02-QueryMusic/src/dev/lpa/MusicDML.java
+++ _22-02-QueryMusic/src/dev/lpa/MusicDML.java
@@ -32,6 +32,8 @@ public class MusicDML {
             if (!executeSelect(statement,tableName, columnName, columnValue)) {
                 System.out.println("Maybe we should add this record");
                 insertRecord(statement, tableName, new String[]{columnName}, new String[]{columnValue});
+            } else {
+                deleteRecord(statement, tableName, columnName, columnValue);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -86,5 +88,18 @@ public class MusicDML {
             executeSelect(statement, table, columnNames[0], columnValues[0]);
         }
         return recordsInserted > 0;
+    }
+
+    private static boolean deleteRecord(Statement statement, String table,
+                                        String columnName, String columnValue) throws SQLException {
+
+        String query = "DELETE FROM %s WHERE %s='%s'".formatted(table, columnName, columnValue);
+        System.out.println(query);
+        statement.execute(query);
+        int recordsDeleted = statement.getUpdateCount();
+        if (recordsDeleted > 0) {
+            executeSelect(statement, table, columnName, columnValue);
+        }
+        return recordsDeleted > 0;
     }
 }
