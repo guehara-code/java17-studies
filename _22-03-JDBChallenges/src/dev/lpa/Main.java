6e3fb43468d9e1c74c33d9d788ea07f8d0ad06f7
guehara-code <gil_uehara@yahoo.com.br>
2024-04-03 15:59:41 -0300

    more studies

--- _22-03-JDBChallenges/src/dev/lpa/Main.java
+++ _22-03-JDBChallenges/src/dev/lpa/Main.java
@@ -3,12 +3,15 @@ package dev.lpa;
 import org.mariadb.jdbc.MariaDbDataSource;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
 
     private static String USE_SCHEMA = "USE storefront";
+
+    private static int MYSQL_DB_NOT_FOUND = 1049;
 
     public static void main(String[] args) {
 
@@ -23,6 +26,8 @@ public class Main {
 
         try (Connection conn = dataSource.getConnection()) {
 
+            DatabaseMetaData metaData = conn.getMetaData();
+            System.out.println(metaData.getSQLStateType());
             if (!checkSchema(conn)) {
                 System.out.println("storefront schema does not exist");
             }
@@ -32,13 +37,20 @@ public class Main {
 
     }
 
-    private static boolean checkSchema(Connection conn) {
+    private static boolean checkSchema(Connection conn) throws SQLException {
 
         try (Statement statement = conn.createStatement()) {
             statement.execute(USE_SCHEMA);
         } catch (SQLException e) {
             e.printStackTrace();
-            return false;
+            System.err.println("SQLState: " + e.getSQLState());
+            System.err.println("Error Code: " + e.getErrorCode());
+            System.err.println("Message: " + e.getMessage());
+
+            if (conn.getMetaData().getDatabaseProductName().equals("MariaDB")
+            && e.getErrorCode() == MYSQL_DB_NOT_FOUND) {
+                return false;
+            } else throw e;
         }
         return true;
     }
