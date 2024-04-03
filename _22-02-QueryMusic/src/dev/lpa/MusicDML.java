1a5080abb1a73cddf67fcfa808799d89e27e770a
guehara-code <gil_uehara@yahoo.com.br>
2024-04-03 09:28:13 -0300

    more studies

--- _22-02-QueryMusic/src/dev/lpa/MusicDML.java
+++ _22-02-QueryMusic/src/dev/lpa/MusicDML.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.sql.*;
+import java.util.Arrays;
 
 public class MusicDML {
 
@@ -175,21 +176,34 @@ public class MusicDML {
 
     private static void deleteArtistAlbum(Connection conn, Statement statement,
                                      String artistName, String albumName) throws SQLException {
-        System.out.println("AUTOCOMMIT = " + conn.getAutoCommit());
-        String deleteSongs = """
-                DELETE FROM music.songs WHERE album_id =
-                (SELECT ALBUM_ID from music.albums WHERE album_name = '%s')"""
-                .formatted(albumName);
-        int deletedSongs = statement.executeUpdate(deleteSongs);
-        System.out.printf("Deleted %d rows from music.songs%n", deletedSongs);
-        String deleteAlbums = "DELETE FROM music.albums WHERE album_name='%s'"
-                .formatted(albumName);
-        int deletedAlbums = statement.executeUpdate(deleteAlbums);
-        System.out.printf("Deleted %d rows form music.albums%n", deletedAlbums);
-        String deleteArtist = "DELETE FROM music.artists WHERE artist_name='%s'"
-                .formatted(artistName);
-        int deletedArtists = statement.executeUpdate(deleteArtist);
-        System.out.printf("Deleted %d rows form music.artists%n", deletedArtists);
+        try {
+            System.out.println("AUTOCOMMIT = " + conn.getAutoCommit());
+            conn.setAutoCommit(false);
+            String deleteSongs = """
+                    DELETE FROM music.songs WHERE album_id =
+                    (SELECT ALBUM_ID from music.albums WHERE album_name = '%s')"""
+                    .formatted(albumName);
+//            int deletedSongs = statement.executeUpdate(deleteSongs);
+//            System.out.printf("Deleted %d rows from music.songs%n", deletedSongs);
+            String deleteAlbums = "DELETE FROM music.albums WHERE album_name='%s'"
+                    .formatted(albumName);
+//            int deletedAlbums = statement.executeUpdate(deleteAlbums);
+//            System.out.printf("Deleted %d rows form music.albums%n", deletedAlbums);
+            String deleteArtist = "DELETE FROM music.artists WHERE artist_name='%s'"
+                    .formatted(artistName);
+//            int deletedArtists = statement.executeUpdate(deleteArtist);
+//            System.out.printf("Deleted %d rows form music.artists%n", deletedArtists);
+            statement.addBatch(deleteSongs);
+            statement.addBatch(deleteAlbums);
+            statement.addBatch(deleteArtist);
+            int[] results = statement.executeBatch();
+            System.out.println(Arrays.toString(results));
+            conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            conn.rollback();
+        }
+        conn.setAutoCommit(true);
     }
 
 }
