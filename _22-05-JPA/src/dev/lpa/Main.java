aa09f156f4e38c436e9b0266765210c8112cf4ff
guehara-code <gil_uehara@yahoo.com.br>
2024-04-08 13:20:09 -0300

    more studies

--- _22-05-JPA/src/dev/lpa/Main.java
+++ _22-05-JPA/src/dev/lpa/Main.java
@@ -1,9 +1,25 @@
 package dev.lpa;
 
+import dev.lpa.music.Artist;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        try(var sessionFactory = Persistence.createEntityManagerFactory(
+                "dev.lpa.music");
+            EntityManager entityManager = sessionFactory.createEntityManager();
+        ) {
 
+            var transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(new Artist("Muddy Water"));
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
