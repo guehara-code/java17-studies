6472c0da240d47dfdcc991a043be21857e9c17dc
guehara-code <gil_uehara@yahoo.com.br>
2024-04-09 21:08:27 -0300

    more studies

--- _22-05-JPA/src/dev/lpa/MainQuery.java
+++ _22-05-JPA/src/dev/lpa/MainQuery.java
@@ -5,8 +5,13 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.Tuple;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 import java.util.List;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainQuery {
@@ -21,6 +26,20 @@ public class MainQuery {
             transaction.begin();
             artists = getArtistJPQL(em, "%Greatest Hits%");
             artists.forEach(System.out::println);
+
+            System.out.println("-------------------------------------------------");
+
+//            Stream<Artist> sartists = getArtistsBuilder(em, "Bl%");
+            Stream<Artist> sartists = getArtistsSQL(em, "Bl%");
+            var map = sartists
+                    .limit(10)
+                    .collect(Collectors.toMap(
+                            Artist::getArtistName,
+                            (a) -> a.getAlbums().size(),
+                            Integer::sum,
+                            TreeMap::new
+                    ));
+            map.forEach((k, v) -> System.out.println(k + " : " + v));
 
 //            var names = getArtistNames(em, "%Stev%");
 ////            names.forEach(System.out::println);
@@ -56,6 +75,26 @@ public class MainQuery {
                 "OR album.albumName LIKE ?2";
         var query = em.createQuery(jpql, Tuple.class);
 //        query.setParameter("partialName", matchedValue);
+        query.setParameter(1, matchedValue);
+        return query.getResultStream();
+    }
+
+    private static Stream<Artist> getArtistsBuilder(EntityManager em,
+                                                    String matchedValue) {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Artist> criteriaQuery = builder.createQuery(Artist.class);
+        Root<Artist> root = criteriaQuery.from(Artist.class);
+        criteriaQuery
+                .select(root)
+                .where(builder.like(root.get("artistName"), matchedValue))
+                .orderBy(builder.asc(root.get("artistName")));
+        return em.createQuery(criteriaQuery).getResultStream();
+    }
+
+    private static Stream<Artist> getArtistsSQL(EntityManager em, String matchedValue) {
+
+        var query = em.createNativeQuery(
+                "SELECT * FROM music.artists WHERE artist_name like ?1", Artist.class);
         query.setParameter(1, matchedValue);
         return query.getResultStream();
     }
