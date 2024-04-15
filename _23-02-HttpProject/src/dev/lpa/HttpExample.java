a4c64528a30032be7548827454bfd2596d71392f
guehara-code <gil_uehara@yahoo.com.br>
2024-04-15 09:28:14 -0300

    more studies

--- _23-02-HttpProject/src/dev/lpa/HttpExample.java
+++ _23-02-HttpProject/src/dev/lpa/HttpExample.java
@@ -15,7 +15,8 @@ public class HttpExample {
     public static void main(String[] args) {
 
         try {
-            URL url = new URL("http://example.com");
+//            URL url = new URL("http://example.com/extra");
+            URL url = new URL("http://localhost:8080");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("User-Agent", "Chrome");
@@ -26,6 +27,7 @@ public class HttpExample {
             System.out.printf("Response code: %d%n", responseCode);
             if (responseCode != HTTP_OK) {
                 System.out.println("Error reading web page " + url);
+                System.out.printf("Error: %s%n", connection.getResponseMessage());
                 return;
             }
             printContents(connection.getInputStream());
