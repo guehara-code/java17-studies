467afadaf5f5b8025abd322b855e0bea22ca6341
guehara-code <gil_uehara@yahoo.com.br>
2024-04-20 08:07:30 -0300

    more studies

--- _24-02-Banking/src/dev/lpa/BankAccountTest.java
+++ _24-02-Banking/src/dev/lpa/BankAccountTest.java
@@ -6,13 +6,17 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
+        fail("This test has yet to be implemented");
     }
 
     @org.junit.Test
     public void withdraw() {
+        fail("This test has yet to be implemented");
     }
 
     @org.junit.Test
     public void getBalance() {
+        fail("This test has yet to be implemented");
     }
+
 }
