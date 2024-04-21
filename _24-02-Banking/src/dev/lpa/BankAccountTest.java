182ce199ac75e96494e9e36186d47fb57d8b5fba
guehara-code <gil_uehara@yahoo.com.br>
2024-04-21 16:34:39 -0300

    more studies

--- _24-02-Banking/src/dev/lpa/BankAccountTest.java
+++ _24-02-Banking/src/dev/lpa/BankAccountTest.java
@@ -4,9 +4,16 @@ import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    private BankAccount account;
+
+    @org.junit.Before
+    public void setup() {
+        account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
+
     @org.junit.Test
     public void deposit() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         double balance = account.deposit(200.00, true);
         assertEquals(1200.00, balance, 0);
     }
@@ -18,21 +25,18 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance_deposit() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void getBalance_withdraw() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         account.withdraw(200.00, true);
         assertEquals(800.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void isChecking_true() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         assertTrue("The account is NOT a checking account", account.isChecking());
     }
 
