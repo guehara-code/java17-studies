170408a8d3d7a380b3c25526372f22bc8e20a463
guehara-code <gil_uehara@yahoo.com.br>
2024-04-25 08:59:42 -0300

    more studies

--- _12-01b-Generics/src/dev/lpa/Team.java
+++ _12-01b-Generics/src/dev/lpa/Team.java
@@ -4,23 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
-public class BaseballTeam {
+public class Team<T> {
 
     private String teamName;
-    private List<BaseballPlayer> teamMembers = new ArrayList<>();
+    private List<T> teamMembers = new ArrayList<>();
     private int totalWins = 0;
     private int totalLosses = 0;
     private int totalTies = 0;
 
-    public BaseballTeam(String teamName) {
+    public Team(String teamName) {
         this.teamName = teamName;
     }
 
-    public void addTeamMember(BaseballPlayer player) {
+    public void addTeamMember(T t) {
 
-        if (!teamMembers.contains(player)) {
-            teamMembers.add(player);
+        if (!teamMembers.contains(t)) {
+            teamMembers.add(t);
         }
     }
 
